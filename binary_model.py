b5627da6fe3819d40884956ecc6163fd0152d2fc
matheguo(郭大山) <matheguo@tencent.com>
2018-07-01 21:48:13 +0800

    debugs

--- binary_model.py
+++ binary_model.py
@@ -5,7 +5,7 @@ from transforms import *
 import torchvision.models
 
 class BinaryClassifier(torch.nn.Module):
-    def __init__(self, num_class, course_segment,dropout=0.8, test_mode=False):
+    def __init__(self, num_class, course_segment, input_dim, dropout=0.8, test_mode=False):
 
         super(BinaryClassifier, self).__init__()
         self.num_segments = course_segment
@@ -13,7 +13,7 @@ class BinaryClassifier(torch.nn.Module):
         self.reshape = True
         self.dropout = dropout
         self.test_mode = test_mode
-        self.binary_classifier = nn.Linear(feature_dim, num_class)
+        self.binary_classifier = nn.Linear(input_dim, num_class)
 
     def forward(self, inputdata, target):
         if not self.test_mode:
@@ -23,17 +23,11 @@ class BinaryClassifier(torch.nn.Module):
 
 
     def train_forward(self, inputdata, target):
-        sample_len = (3 if self.modality == "RGB" else 2) * self.new_length
-        base_out = self.base_model(inputdata.view((-1, sample_len) + inputdata.size()[-2:]))
-        src = base_out.view(-1, self.course_segment, base_out.size()[1])
-        course_ft = src[:, :, :].mean(dim=1)
+        course_ft = input_data[:, :, :].mean(dim=1)
         raw_course_ft = self.classifier_fc(course_ft)
         target = target.view(-1)
-
         return raw_course_ft, target
                 
 
     def test_forward(self, input):
-        sample_len = (3 if self.modality == 'RGB' else 2) * self.new_length
-        base_out = self.base_model(input.view((-1,sample_len) + input.size()[-2:]))
-        return self.test_fc(base_out), base_out
+        return self.self.classifier_fc(input), input
