915af8b31624035d573411e8ed21185eb76a67b4
matheguo(郭大山) <matheguo@tencent.com>
2018-07-02 11:44:39 +0800

    compute accuracy when batch training

--- binary_train.py
+++ binary_train.py
@@ -257,6 +257,7 @@ def accuracy(output, target, topk=(1,)):
 
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
+    print(output.size(), pred.size(), target.size())
     correct = pred.eq(target.view(1, -1).expand_as(pred))
 
     res = []
