d09e06efbbddb17b2d2863bc1a21eabb207ea255
matheguo(郭大山) <matheguo@tencent.com>
2018-07-02 16:06:49 +0800

    repair the test code

--- binary_test.py
+++ binary_test.py
@@ -88,7 +88,7 @@ def runner_func(dataset, state_dict, gpu_id, index_queue, result_queue):
     while True:
         index = index_queue.get()
         frames_gen, frame_cnt = dataset[index]
-        input_feat = Variable(frames_gen)
+        input_feat = Variable(frames_gen).cuda()
         with torch.no_grad():
             output = net(input_feat, None)
         print(output[0, 0])
