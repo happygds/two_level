e4497246e0be81b9f8f97ae8426ef6cbed1822f2
matheguo(郭大山) <matheguo@tencent.com>
2018-07-09 11:23:34 +0800

    add codes for sparsemax

--- attention/Modules.py
+++ attention/Modules.py
@@ -71,8 +71,7 @@ class ScaledDotProductAttention(nn.Module):
         if self.kernel_type in ['self_attn', 'addition']:
             # attn = self.softmax(attn)
             shp = attn.size()
-            lengths = (1. - attn_mask).sum(1).long().cuda()
-            print(lengths.size())
+            lengths = (1. - attn_mask).sum(1).long().cuda().view(-1)
             attn = self.softmax(attn.view(-1, shp[2]).data.cpu(), lengths.data.cpu()).view(shp).cuda()
             import pdb
             pdb.set_trace()
