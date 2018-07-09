7ce7531c8923e66454594a41100b74d4a2cdef30
matheguo(郭大山) <matheguo@tencent.com>
2018-07-09 11:11:44 +0800

    add codes for sparsemax

--- attention/Modules.py
+++ attention/Modules.py
@@ -64,14 +64,14 @@ class ScaledDotProductAttention(nn.Module):
                     'with Attention logit tensor shape ' \
                     '{}.'.format(attn_mask.size(), attn.size())
             if self.kernel_type in ['self_attn', 'addition']:
-                attn.data.masked_fill_(attn_mask, -1e+32)
+                attn.data.masked_fill_(attn_mask, -'inf')
             else:
                 attn.data.masked_fill_(attn_mask, 0)
 
         if self.kernel_type in ['self_attn', 'addition']:
             # attn = self.softmax(attn)
             shp = attn.size()
-            lengths = (torch.ones(shp[0]*shp[1]) * shp[2]).long().cuda()
+            lengths = (1. - attn).sum(1).long().cuda()
             attn = self.softmax(attn.view(-1, shp[2]).data.cpu(), lengths.data.cpu()).view(shp).cuda()
             import pdb
             pdb.set_trace()
