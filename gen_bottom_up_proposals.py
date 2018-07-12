0a9d5323f6370b3de6e79633d97093570f47f92f
matheguo(郭大山) <matheguo@tencent.com>
2018-07-12 19:05:36 +0800

    add local operations

--- gen_bottom_up_proposals.py
+++ gen_bottom_up_proposals.py
@@ -51,7 +51,7 @@ else:
 def compute_frame_count(video_info, frame_path, name_pattern):    
     # first count frame numbers
     try:
-        video_name = video_info.path.split('/')[-1].split('.')[0]
+        video_name = video_info.id
         print(video_name)
         files = glob.glob(os.path.join(frame_path, video_name, name_pattern))
         frame_cnt = len(files)
