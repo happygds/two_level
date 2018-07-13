735a32d0b1941fb5e4c9f27ef6fd5d81ad032d7e
matheguo(郭大山) <matheguo@tencent.com>
2018-07-13 21:35:53 +0800

    local before non-local

--- attention/Modules.py
+++ attention/Modules.py
@@ -68,15 +68,16 @@ class ScaledDotProductAttention(nn.Module):
             else:
                 attn.data.masked_fill_(attn_mask, 0)
 
-        import pdb
-        pdb.set_trace()
         if self.kernel_type in ['self_attn', 'addition', 'inner_prod']:
             attn = self.softmax(attn)
+            attn.data.masked_fill_(torch.isnan(attn), 0)
             # shp = attn.size()
             # lengths = (1. - attn_mask)[:, 0].sum(-1).long().cuda()
             # attn = self.softmax(attn.data.cpu(), lengths.data.cpu()).view(shp).cuda()
         else:
             attn = attn / attn.sum(dim=2, keepdim=True).clamp(1e-14)
+        import pdb
+        pdb.set_trace()
         attn = self.dropout(attn)
         output = torch.bmm(attn, v)
 
