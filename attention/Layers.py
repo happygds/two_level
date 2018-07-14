8e7ba3dec6033c3f95cd07720961ef7958c40c1a
matheguo(郭大山) <matheguo@tencent.com>
2018-07-14 19:38:53 +0800

    local as query

--- attention/Layers.py
+++ attention/Layers.py
@@ -130,6 +130,6 @@ class Local_EncoderLayer(nn.Module):
         local_output, local_attn = self.local_attn(
             enc_input, enc_input, enc_input, attn_mask=local_attn_mask)
         enc_output, enc_slf_attn = self.slf_attn(
-            local_output, enc_input, enc_input, attn_mask=slf_attn_mask)
+            local_output, local_output, enc_input, attn_mask=slf_attn_mask)
         enc_output = self.pos_ffn(enc_output)
         return local_output, enc_slf_attn
