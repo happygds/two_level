db6d9edc9003271020eed192f31e35ddd7758263
matheguo(郭大山) <matheguo@tencent.com>
2018-07-21 21:33:33 +0800

    change clustering

--- attention/Layers.py
+++ attention/Layers.py
@@ -73,14 +73,12 @@ class Cluster_EncoderLayer(nn.Module):
 
     def __init__(self, d_model, d_inner_hid, n_head, d_k, d_v, dropout=0.1, kernel_type='self_attn', n_cluster=64, local_type=None):
         super(Cluster_EncoderLayer, self).__init__()
-        self.local_type = local_type
-        self.local_attn = MultiHeadAttention(
-            n_head//4, d_model, d_k*4, d_v*4, dropout=dropout, kernel_type=kernel_type)
+        # self.local_type = local_type
+        # self.local_attn = MultiHeadAttention(
+        #     n_head//4, d_model, d_k*4, d_v*4, dropout=dropout, kernel_type=kernel_type)
 
         self.assign_attn = MultiHeadAttention(
-            n_head, d_model, d_k, d_v, dropout=dropout, kernel_type=kernel_type)
-        self.assign_fc = nn.Linear(d_model, n_cluster)
-        self.assignT_fc = nn.Linear(d_model, n_cluster)
+            n_head, d_model, d_k, d_v, d_out=n_cluster, dropout=dropout, kernel_type=kernel_type)
         self.assign_softmax = nn.Softmax(dim=2)
         # self.assign_softmax = Sparsemax(mask_value=-1e+32)
 
@@ -92,38 +90,34 @@ class Cluster_EncoderLayer(nn.Module):
         # for non-local operation
         self.cluster_attn = MultiHeadAttention(
             n_head, d_model, d_k, d_v, dropout=dropout, kernel_type=kernel_type)
-        # self.pos_ffn_cluster = PositionwiseFeedForward(
-        #     d_model, d_inner_hid, dropout=dropout)
+        self.slf_attn_2 = MultiHeadAttention(
+            n_head, d_model, d_k, d_v, dropout=dropout, kernel_type=kernel_type)
         
         self.pos_ffn = PositionwiseFeedForward(
             d_model, d_inner_hid, d_in=d_model*2, dropout=dropout)
 
     def forward(self, enc_input, local_attn_mask=None, slf_attn_mask=None):
-        local_output, local_attn = self.local_attn(
-            enc_input, enc_input, enc_input, attn_mask=local_attn_mask)
+        # local_output, local_attn = self.local_attn(
+        #     enc_input, enc_input, enc_input, attn_mask=local_attn_mask)
 
         enc_slf_output, enc_slf_attn = self.slf_attn(
-            local_output, local_output, local_output, attn_mask=slf_attn_mask)
+            enc_input, enc_input, enc_input, attn_mask=slf_attn_mask)
         # enc_slf_output = self.pos_ffn_slf(enc_slf_output)
 
-        assign_output, _ = self.assign_attn(
-            local_output, local_output, local_output, attn_mask=slf_attn_mask)
-        assign_mat = self.assign_fc(assign_output)
+        assign_mat, _ = self.assign_attn(
+            enc_input, enc_input, enc_input, attn_mask=slf_attn_mask)
         assign_mask = slf_attn_mask[:, 0].unsqueeze(2).expand(assign_mat.size()).byte()
         assign_mat.data.masked_fill_(assign_mask, -float('inf'))
-        assign_mat = self.assign_softmax(assign_mat)   # mb_size * len_q * n_cluster
+        assign_mat = self.assign_softmax(assign_mat.transpose(1, 2)).transpose(1, 2)   # mb_size * len_q * n_cluster
         cluster_input = torch.bmm(assign_mat.transpose(1, 2), enc_slf_output)
 
         cluster_output, _ = self.cluster_attn(
             cluster_input, cluster_input, cluster_input)
-        assignT_mat = self.assignT_fc(assign_output.transpose(1, 2))
-        assignT_mask = slf_attn_mask[:, 0].unsqueeze(2).expand(assignT_mat.size()).byte()
-        assignT_mat.data.masked_fill_(assignT_mask, -float('inf'))
-        assignT_mat = self.assign_softmax(assignT_mat)   # mb_size * len_q * n_cluster
-        cluster_output = torch.bmm(assignT_mat, cluster_output) + enc_slf_output
-        # cluster_output = self.pos_ffn_cluster(cluster_output)
+        cluster_output = torch.bmm(assign_mat, cluster_output)
+        cluster_output, _ = self.slf_attn_2(
+            cluster_output, cluster_output, cluster_output, attn_mask=slf_attn_mask)
         
-        enc_output = self.pos_ffn(torch.cat((local_output, cluster_output), dim=2))
+        enc_output = self.pos_ffn(torch.cat((cluster_output, enc_slf_output), dim=2))
         # enc_output = self.pos_ffn(enc_output)
         return enc_output, enc_slf_attn
 
