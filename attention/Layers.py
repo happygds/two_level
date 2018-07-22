fe6e84c957e4d9af418674ade4269239bff8a00d
matheguo(郭大山) <matheguo@tencent.com>
2018-07-22 10:32:35 +0800

    simplify the clustering step

--- attention/Layers.py
+++ attention/Layers.py
@@ -92,7 +92,7 @@ class Cluster_EncoderLayer(nn.Module):
             n_head, d_model, d_k, d_v, dropout=dropout, kernel_type=kernel_type)
         
         self.pos_ffn = PositionwiseFeedForward(
-            d_model, d_inner_hid, d_in=d_model, dropout=dropout)
+            d_model, d_inner_hid, d_in=2*d_model, dropout=dropout)
 
     def forward(self, enc_input, local_attn_mask=None, slf_attn_mask=None):
         # local_output, local_attn = self.local_attn(
@@ -106,12 +106,12 @@ class Cluster_EncoderLayer(nn.Module):
         cluster_input = torch.bmm(assign_mat.transpose(1, 2), enc_slf_output)
 
         cluster_output, _ = self.cluster_attn(
-            cluster_input, cluster_input, cluster_input)
-        cluster_output = torch.bmm(assign_mat, cluster_output) + enc_slf_output
-        enc_output, enc_slf_attn = self.slf_attn(
-            cluster_output, cluster_output, cluster_output, attn_mask=local_attn_mask)
+            enc_slf_output, cluster_input, cluster_input)
+        cluster_output = torch.bmm(assign_mat, cluster_output)
+        # enc_output, enc_attn = self.slf_attn(
+        #     cluster_output, cluster_output, cluster_output, attn_mask=local_attn_mask)
         
-        # enc_output = self.pos_ffn(torch.cat((cluster_output, enc_slf_output), dim=2))
-        enc_output = self.pos_ffn(enc_output)
-        return enc_output, enc_slf_attn
+        enc_output = self.pos_ffn(torch.cat((cluster_output, enc_slf_output), dim=2))
+        # enc_output = self.pos_ffn(enc_output)
+        return enc_output, enc_attn
 
