dcec83f79eb389650908c97e78293fe9d16bbace
matheguo(郭大山) <matheguo@tencent.com>
2018-07-23 10:39:37 +0800

    use scheduled adam

--- binary_train.py
+++ binary_train.py
@@ -195,14 +195,14 @@ def train(train_loader, model, criterion, optimizer, epoch):
         end = time.time()
 
         if i % args.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]'
+            print('Epoch: [{0}][{1}/{2}], lr: {lr:.5f}\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                   '\n FG{fg_acc.val:.02f}({fg_acc.avg:.02f}) BG {bg_acc.val:.02f} ({bg_acc.avg:.02f})'
                   .format(
                       epoch, i, len(train_loader), batch_time=batch_time,
-                      data_time=data_time, loss=losses,
+                      data_time=data_time, loss=losses, lr=optimizer.optimizer.param_groups[0]['lr'],
                       fg_acc=fg_accuracies, bg_acc=bg_accuracies)
                   )
 
