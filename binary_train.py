3b167642f1fc24ba92e784bc5c4f8be27718e650
matheguo(郭大山) <matheguo@tencent.com>
2018-07-23 21:51:27 +0800

    remove sampling feature

--- binary_train.py
+++ binary_train.py
@@ -207,7 +207,8 @@ def validate(val_loader, model, criterion, iter):
             target = convert_categorical(target.cpu().numpy(), n_classes=2)
             target = torch.from_numpy(target).cuda().requires_grad_(False)
             target *= feature_mask.unsqueeze(2)
-            cls_weight = 1. / target.mean(0).mean(0)
+            # cls_weight = 1. / target.mean(0).mean(0)
+            cls_weight = (feature_mask.sum(1).unsqueeze(1) / target.sum(1)).mean(0)
 
             # compute output
             binary_score = model(feature, pos_ind, feature_mask=feature_mask)
