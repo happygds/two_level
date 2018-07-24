24d35ed192f2139ee1ae96acf724bdbc0e130b9d
matheguo(郭大山) <matheguo@tencent.com>
2018-07-24 17:41:14 +0800

    debug weight

--- binary_train.py
+++ binary_train.py
@@ -144,8 +144,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
         target *= feature_mask.unsqueeze(2)
         # cls_weight = 1. / target.mean(0).mean(0)
         cls_weight = target.sum(1) / feature_mask.sum(1).unsqueeze(1)
-        cls_weight = 0.5 / cls_weight.mean(0)
-        cls_weight[1] *= 2
+        cls_weight = 0.5 / cls_weight.clamp(0.01)
 
         # compute output
         binary_score = model(feature, pos_ind, feature_mask=feature_mask)
