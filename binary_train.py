a4d8be175561b64700f45e637a6e5ee72eab796d
matheguo(郭大山) <matheguo@tencent.com>
2018-07-24 15:12:02 +0800

    debug weight

--- binary_train.py
+++ binary_train.py
@@ -203,6 +203,8 @@ def validate(val_loader, model, criterion, iter):
             target *= feature_mask.unsqueeze(2)
             # cls_weight = 1. / target.mean(0).mean(0)
             cls_weight = target.sum(1) / feature_mask.sum(1).unsqueeze(1)
+            import pdb
+            pdb.set_trace()
             cls_weight = (0.5 / cls_weight).mean(0)
 
             # compute output
