a4821682f05b86747ebae2e63422f0840b5add8c
matheguo(郭大山) <matheguo@tencent.com>
2018-07-26 16:48:26 +0800

    add highorder

--- attention/Modules.py
+++ attention/Modules.py
@@ -40,11 +40,9 @@ class ScaledDotProductAttention(nn.Module):
         elif self.kernel_type == 'addition':
             self.fc = nn.Sequential(nn.Tanh(), nn.Linear(d_k, 1))
         elif self.kernel_type == 'highorder':
-            self.conv_layers = nn.Sequential(nn.Conv2d(self.n_head, 8*self.n_head, 3, padding=1),
-                                             nn.BatchNorm2d(8*self.n_head), nn.ReLU(),
-                                            #  nn.Conv2d(32, 32, 3, padding=1), nn.ReLU(),
-                                             nn.Conv2d(8*self.n_head, self.n_head, 3, padding=1),
-                                             nn.BatchNorm2d(self.n_head))
+            self.conv_layers = nn.Sequential(nn.Conv2d(self.n_head, 8*self.n_head, 3, padding=1), nn.ReLU(),
+                                             nn.Conv2d(8*self.n_head, 8*self.n_head, 1, padding=0), nn.ReLU(),
+                                             nn.Conv2d(8*self.n_head, self.n_head, 3, padding=1),)
 
     def forward(self, q, k, v, attn_mask=None):
         if self.kernel_type == 'self_attn':
