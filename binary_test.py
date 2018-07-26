a98da782ff30739cc4c7cf22447118fa96c63eec
matheguo(郭大山) <matheguo@tencent.com>
2018-07-26 22:27:01 +0800

    add highorder

--- binary_test.py
+++ binary_test.py
@@ -121,8 +121,11 @@ def runner_func(dataset, state_dict, gpu_id, index_queue, result_queue):
         feature = torch.autograd.Variable(feature).cuda()
         pos_ind = torch.autograd.Variable(pos_ind).cuda()
         with torch.no_grad():
-            output = net(feature, pos_ind)
-            output = output[0].cpu().numpy()
+            if feature.size[1] > 1:
+                output = net(feature, pos_ind)
+                output = output[0].cpu().numpy()
+            else:
+                output = np.asarray([0, 1]).reshape((1, 1)).astype('float32')
         # nframes = len(output) * args.frame_interval
         # output = np.interp(
         #     np.arange(nframes), np.arange(nframes)[::args.frame_interval] + args.frame_interval / 2 - 0.5, output[:, 1])
