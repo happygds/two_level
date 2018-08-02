be9f0922761bc03b2020127de4b50c5ff7ee3027
matheguo(郭大山) <matheguo@tencent.com>
2018-08-02 17:13:28 +0800

    try 1-order diff

--- binary_model.py
+++ binary_model.py
@@ -146,13 +146,23 @@ class BinaryClassifier(torch.nn.Module):
             enc_output, enc_slf_attn = enc_layer(
                 enc_output, local_attn_mask=local_attn_mask, slf_attn_mask=enc_slf_attn_mask)
             enc_slf_attns += [enc_slf_attn]
-        score_output = self.softmax(self.binary_classifier(enc_output))
 
-        # # use scores embedding
-        # score_embed = score_embedding(score_output[:, :, 1], self.d_model)
-        # score_att_output, _ = self.score_att_layer(
-        #         score_embed, slf_attn_mask=enc_slf_attn_mask)
-        # score_att_output = self.softmax(self.binary_score(score_att_output))
+        enc_output_diff = enc_output[:, 1:, :] - enc_output[:, :-1, :]
+        if feature_mask is not None:
+            enc_slf_attn_mask = (
+                1. - feature_mask[:, 1:]).unsqueeze(1).expand(mb_size, len_k, len_k).byte()
+        else:
+            enc_slf_attn_mask = torch.zeros((mb_size, len_k-1, len_k-1)).byte().cuda()
+        local_attn_mask = None
+        if self.num_local > 0:
+            local_attn_mask = get_attn_local_mask(enc_slf_attn_mask, num_local=self.num_local)
+            if self.dilated_mask:
+                enc_slf_attn_mask = get_attn_dilated_mask(enc_slf_attn_mask, num_local=self.num_local)
+        enc_output_diff, _ = self.score_att_layer(
+            enc_output_diff, local_attn_mask=local_attn_mask, slf_attn_mask=enc_slf_attn_mask)
+        enc_output[:, 1:, :] = enc_output[:, :-1, :] + enc_output_diff
+
+        score_output = self.softmax(self.binary_classifier(enc_output))
 
         return score_output
 
