ec70078962b4a0f9e073695683998c8205106cd0
matheguo(郭大山) <matheguo@tencent.com>
2018-08-03 14:26:09 +0800

    add diff output score

--- binary_test.py
+++ binary_test.py
@@ -127,13 +127,14 @@ def runner_func(dataset, state_dict, gpu_id, index_queue, result_queue):
             if isinstance(output, list):
                 output = output[-1]
             output = output[0].cpu().numpy()[:num_feat]
-            if args.use_diff_score:
+            if args.use_diff_score and num_feat > 1:
                 output_diff = output_diff[0].cpu().numpy()[:(num_feat-1)]
                 output_diff = output_diff[:, 0] - output_diff[:, 2]
                 tmp, tmp_output = output[:, 1].copy(), output.copy()
                 tmp[1:] = tmp[:-1] + output_diff
                 tmp_output[:, 1] = tmp
                 tmp_output[:, 0] = 1. - tmp_output[:, 1]
+                output = tmp_output
         # nframes = len(output) * args.frame_interval
         # output = np.interp(
         #     np.arange(nframes), np.arange(nframes)[::args.frame_interval] + args.frame_interval / 2 - 0.5, output[:, 1])
