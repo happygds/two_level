3aef2bded9ec57a63468b63c37d71a53225faa73
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-10 20:13:33 +0800

    resize input squence into a fixed length

--- binary_test.py
+++ binary_test.py
@@ -133,7 +133,7 @@ def runner_func(dataset, state_dict, gpu_id, index_queue, result_queue):
             outputs = net(feature, pos_ind, feature_mask=feature_mask, test_mode=True)
             outputs = [output[0].cpu().numpy()[:num_feat] for _, output in enumerate(outputs)]
         import time
-        print(outputs[:, 1])
+        print(outputs[0][:, 1])
         time.sleep(100)
 
         result_queue.put((dataset.video_list[index].id.split('/')[-1], outputs))
