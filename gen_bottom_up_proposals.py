c14dad8107beec978399190a3362fe9986fe9b88
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-11 11:37:48 +0800

    try different thresholds

--- gen_bottom_up_proposals.py
+++ gen_bottom_up_proposals.py
@@ -169,7 +169,7 @@ def gen_prop(v):
 
     # filter out too short proposals
     pr_box = list(filter(lambda b: b[1] - b[0] > args.minimum_len, pr_box))
-    return v.id, pr_box, [x[3] / float(x[1] - x[0]) for x in bboxes]
+    return v.id, pr_box, [x[3] for x in bboxes]
 
 
 def call_back(rst):
