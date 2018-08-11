a561b5287b98fb12b7eaa2aff2c7983e1cae9470
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-11 16:03:11 +0800

    resize input length

--- attention/Modules.py
+++ attention/Modules.py
@@ -98,7 +98,8 @@ class ScaledDotProductAttention(nn.Module):
             attn = torch.bmm(q, k.transpose(1, 2)) / self.temper
             if attn_pos_emb is not None:
                 k_pos_emb, v_pos_emb = torch.split(attn_pos_emb, q.size(2), dim=3)
-                attn += torch.sum(q.unsqueeze(2) * k_pos_emb, dim=3) / self.temper
+                k_pos_gate = F.sigmoid(torch.sum(q.unsqueeze(2) * k_pos_emb, dim=3) / self.temper)
+                attn *=  k_pos_gate
         elif self.kernel_type == 'dot':
             attn = torch.bmm(q, k.transpose(1, 2))
         elif self.kernel_type == 'concat':
