567cf1ae25a6ff8d9e1cd7f502e26000301b3a2e
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-12 11:29:49 +0800

    save result to csv file

--- gen_bottom_up_proposals.py
+++ gen_bottom_up_proposals.py
@@ -233,7 +233,8 @@ prediction = pd.DataFrame({'video-id': video_lst,
                             't-start': t_start_lst,
                             't-end': t_end_lst,
                             'score': score_lst})
-# prediction.to_csv('val.csv')
+dir_path = os.path.split(args.score_files[0])[1]
+prediction.to_csv(os.path.join(dir_path, 'val.csv'))
 
 # prediction.to_csv(os.path.join(opt.result_path, '{}.csv'.format('val')))
 ground_truth, cls_to_idx = grd_activity('/data1/matheguo/important/data/activitynet/activity_net.v1-3.min_save.json', subset='validation')
