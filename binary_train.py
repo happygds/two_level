4bd2b741ecb0a4518545b948390aebaef165d8bd
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-17 19:50:03 +0800

    add kernel alignment loss

--- binary_train.py
+++ binary_train.py
@@ -187,7 +187,7 @@ def train(train_loader, model, criterion, optimizer, epoch, logger):
         binary_score, out_attns = model(feature, pos_ind, feature_mask=feature_mask)
         loss, attn_loss = criterion(binary_score, target, attns=out_attns, mask=feature_mask, 
                          multi_strides=multi_strides)
-        loss += attn_loss * 0.1
+        # loss += attn_loss * 0.1
         attn_losses.update(attn_loss.item(), feature.size(0))
         losses.update(loss.item(), feature.size(0))
 
@@ -254,7 +254,7 @@ def validate(val_loader, model, criterion, iter):
             binary_score, out_attns = model(feature, pos_ind, feature_mask=feature_mask)
             loss, attn_loss = criterion(binary_score, target, attns=out_attns, mask=feature_mask, 
                                         multi_strides=multi_strides)
-            loss += attn_loss * 0.1
+            # loss += attn_loss * 0.1
         attn_losses.update(attn_loss.item(), feature.size(0))
         losses.update(loss.item(), feature.size(0))
 
