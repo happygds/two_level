b5325a6d9aadeac5c8c168befd97abf904bd74ff
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-18 18:39:51 +0800

    try multiple layers stacked

--- binary_test.py
+++ binary_test.py
@@ -132,7 +132,7 @@ def runner_func(dataset, state_dict, gpu_id, index_queue, result_queue):
         with torch.no_grad():
             outputs, _ = net(feature, pos_ind, feature_mask=feature_mask, test_mode=True)
             outputs = [output[0].cpu().numpy()[:num_feat] for _, output in enumerate(outputs)]
-            outputs = [np.asarray(outputs).mean(0)]
+            # outputs = [np.asarray(outputs).mean(0)]
 
         result_queue.put((dataset.video_list[index].id.split('/')[-1], outputs))
         
