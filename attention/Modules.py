56985350d091ce846337bcfaec1283ce5d8a533e
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-21 14:40:59 +0800

    implement groupwise multiheads self-attention

--- attention/Modules.py
+++ attention/Modules.py
@@ -251,9 +251,11 @@ class MultiHeadAttention(nn.Module):
             trn_kernel = self.groupwise_heads
             assert self.n_head % 4 == 0
             k_head = self.n_head // 4 * mb_size
-            q_s[k_head:2*k_head] = F.avg_pool1d(F.pad(q_s[k_head:2*k_head].transpose(1, 2), (trn_kernel-1, 0)), trn_kernel).transpose(1, 2)
-            q_s[2*k_head:3*k_head] = F.avg_pool1d(F.pad(q_s[2*k_head:3*k_head].transpose(1, 2), (0, trn_kernel-1)), trn_kernel).transpose(1, 2)
-            q_s[3*k_head:4*k_head] = F.avg_pool1d(F.pad(q_s[3*k_head:4*k_head].transpose(1, 2), ((trn_kernel-1)//2, (trn_kernel-1)//2)), trn_kernel).transpose(1, 2)
+            q_s1, q_s2, q_s3, q_s4 = torch.split(q_s, k_head, dim=0)
+            q_s2 = F.avg_pool1d(F.pad(q_s2.transpose(1, 2), (trn_kernel-1, 0)), trn_kernel).transpose(1, 2)
+            q_s3 = F.avg_pool1d(F.pad(q_s3.transpose(1, 2), (0, trn_kernel-1)), trn_kernel).transpose(1, 2)
+            q_s4 = F.avg_pool1d(F.pad(q_s4.transpose(1, 2), ((trn_kernel-1)//2, (trn_kernel-1)//2)), trn_kernel).transpose(1, 2)
+            q_s = torch.cat([q_s1, q_s2, q_s3, q_s4], dim=0)
 
         if attn_pos_emb is not None:
             attn_pos_emb = attn_pos_emb.repeat(n_head, 1, 1, 1)
