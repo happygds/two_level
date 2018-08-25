690ba8f61b699ffc8b22139b44f639e05f4fb8b4
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-25 19:14:35 +0800

    implement relation based roi removal module

--- binary_train.py
+++ binary_train.py
@@ -264,9 +264,9 @@ def validate(val_loader, model, iter):
             feature_mask = feature_mask.cuda().requires_grad_(False)
             pos_ind = pos_ind.cuda().requires_grad_(False)
             loss_list = model(feature, pos_ind, target, gts=gts, feature_mask=feature_mask)
-        ngpu = loss_list.size(0) // 2
-        score_loss, roi_loss = loss_list[::ngpu].mean(), loss_list[1::ngpu].mean()
-        loss = score_loss + 0.1 * roi_loss
+            ngpu = loss_list.size(0) // 2
+            score_loss, roi_loss = loss_list[::ngpu].mean(), loss_list[1::ngpu].mean()
+            loss = score_loss + 0.1 * roi_loss
         score_losses.update(score_loss.item(), feature.size(0))
         roi_losses.update(roi_loss.item(), feature.size(0))
         losses.update(loss.item(), feature.size(0))
