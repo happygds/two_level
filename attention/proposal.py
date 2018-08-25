40e62d1b72a5ffbe87c159ec63cadef99ebf8cfb
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-25 20:17:12 +0800

    compute loss after model forward

--- attention/proposal.py
+++ attention/proposal.py
@@ -82,7 +82,6 @@ def proposal_layer(score_outputs, gts=None, test_mode=False, ss_prob=0.,
     rois_relative_pos[:, :, :, 1] = rois_dura[:, :, np.newaxis] / rois_dura[:, np.newaxis, :].clip(1e-14)
     rois_relative_pos = np.log(rois_relative_pos.clip(1e-3)) * rpn_rois_mask[:, :, np.newaxis, np.newaxis] * rpn_rois_mask[:, np.newaxis, :, np.newaxis]
 
-    import pdb; pdb.set_trace()
     # convert numpy to pytorch
     rpn_rois = torch.from_numpy(rpn_rois.astype('float32')).cuda().requires_grad_(False).to(device_id)
     rpn_rois_mask = torch.from_numpy(rpn_rois_mask.astype('float32')).cuda().requires_grad_(False).to(device_id)
