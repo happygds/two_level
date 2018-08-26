eea5d511c5077fc03c6de4792a44095b336cab8f
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-26 21:45:32 +0800

    the generated rpn_rois in proposal_layer has 3 elements in the last dimension

--- attention/proposal.py
+++ attention/proposal.py
@@ -39,17 +39,17 @@ def proposal_layer(score_outputs, feature_mask, gts=None, test_mode=False, ss_pr
         num_feat = int(feature_mask[k].sum())
         for s in range(len(score_outputs)):
             scores = score_outputs[s][k][:num_feat]
-            # # use TAG
-            # topk_labels = label_frame_by_threshold(scores, topk_cls, bw=bw, thresh=thresh, multicrop=False)
-            # props = build_box_by_search(topk_labels, np.array(tol_lst))
-            # props = [(x[0], x[1], 1, x[3]) for x in props]
-            # use change point
-            scores = scores[:, 1]
-            tmp = gaussian_filter(scores[1:,] - scores[:-1,], bw)
-            std_value = tmp.std()
-            starts = np.nonzero(tmp > std_value)[0] + 1
-            ends = np.nonzero(tmp < -std_value)[0] + 1
-            props = [(x, y, 1, scores[x:y].mean()) for x in starts for y in ends if x < y] + [(0, len(scores), 1, scores.mean())]
+            # use TAG
+            topk_labels = label_frame_by_threshold(scores, topk_cls, bw=bw, thresh=thresh, multicrop=False)
+            props = build_box_by_search(topk_labels, np.array(tol_lst))
+            props = [(x[0], x[1], 1, x[3]) for x in props]
+            # # use change point
+            # scores = scores[:, 1]
+            # tmp = gaussian_filter(scores[1:,] - scores[:-1,], bw)
+            # std_value = tmp.std()
+            # starts = np.nonzero(tmp > std_value)[0] + 1
+            # ends = np.nonzero(tmp < -std_value)[0] + 1
+            # props = [(x, y, 1, scores[x:y].mean()) for x in starts for y in ends if x < y] + [(0, len(scores), 1, scores.mean())]
             bboxes.extend(props)
         # import pdb; pdb.set_trace()
         bboxes = temporal_nms(bboxes, 0.9)[:rpn_post_nms_top]
