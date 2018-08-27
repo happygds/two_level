54450e56e2c6bc1fa4f41e84cbaa20c427bbd70b
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-27 20:15:11 +0800

    add start and end regions

--- attention/Layers.py
+++ attention/Layers.py
@@ -87,7 +87,7 @@ class ROI_Relation(nn.Module):
                  d_k, d_v, dropout=0.1, kernel_type='roi_remov'):
         super(ROI_Relation, self).__init__()
         self.roi_pool = RoI1DPool(roipool_size, 1.)
-        self.start_pool, self.end_pool = RoI1DPool(1, 1.), RoI1DPool(1, 1.)
+        # self.start_pool, self.end_pool = RoI1DPool(1, 1.), RoI1DPool(1, 1.)
         self.rank_fc = nn.Linear(d_model, d_model)
         self.roi_fc = nn.Linear(d_model*(2+roipool_size), d_model)
         # for non-local operation
@@ -98,10 +98,10 @@ class ROI_Relation(nn.Module):
 
     def forward(self, features, start_rois, end_rois, rois, rois_mask, rois_pos_emb):
         inner_feats = self.roi_pool(features.transpose(1, 2), rois)
-        start_feats = self.start_pool(features.transpose(1, 2), start_rois)
-        end_feats = self.end_pool(features.transpose(1, 2), end_rois)
+        # start_feats = self.start_pool(features.transpose(1, 2), start_rois)
+        # end_feats = self.end_pool(features.transpose(1, 2), end_rois)
         roi_feat_size = inner_feats.size()
-        start_feats, end_feats = start_feats.view(roi_feat_size[:2] + (-1,)), end_feats.view(roi_feat_size[:2] + (-1,))
+        # start_feats, end_feats = start_feats.view(roi_feat_size[:2] + (-1,)), end_feats.view(roi_feat_size[:2] + (-1,))
         inner_feats = inner_feats.view(roi_feat_size[:2] + (-1,))
         # roi_feats = torch.cat([start_feats, inner_feats, end_feats], dim=2)
         roi_feats = inner_feats
