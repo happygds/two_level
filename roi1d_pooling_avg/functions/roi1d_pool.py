033c0078b9242ee8aa852bb8373834a4d96b199f
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-27 20:29:55 +0800

    implement roipool

--- roi1d_pooling_avg/functions/roi1d_pool.py
+++ roi1d_pooling_avg/functions/roi1d_pool.py
@@ -17,20 +17,13 @@ class RoI1DPoolFunction(Function):
         output = torch.zeros(batch_size, num_rois, num_channels,
                              self.pooled_depth)
 
-        # if not features.is_cuda:
-        #     _features = features.permute(0, 2, 3, 1)
-        #     roi3d_pooling.roi3d_pooling_forward(self.pooled_height, self.pooled_width, self.spatial_scale,
-        #                                     _features, rois, output)
-        #     # output = output.cuda()
-        # else:
         output = output.cuda()
         roi1d_pooling.roi1d_pooling_forward_cuda(self.pooled_depth, self.temporal_scale, features, rois, output)
-        self.output = output
+        # self.output = output
         self.rois = rois
         self.feature_size = features.size()
-        del output, rois
 
-        return self.output
+        return output
 
     def backward(self, grad_output):
         assert(self.feature_size is not None and grad_output.is_cuda)
@@ -40,7 +33,6 @@ class RoI1DPoolFunction(Function):
         grad_input = torch.zeros(batch_size, num_channels, data_depth).cuda()
         roi1d_pooling.roi1d_pooling_backward_cuda(self.pooled_depth, self.temporal_scale, grad_output,
                                                   self.rois, grad_input)
-
-        # print grad_input
+        self.rois = None
 
         return grad_input, None
