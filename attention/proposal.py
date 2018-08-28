e0921b74b777d5b8c998f0d4c7a81a7b22a88392
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-08-28 20:02:07 +0800

    implement bsn network

--- attention/proposal.py
+++ attention/proposal.py
@@ -60,6 +60,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
             0.5*(scores[max(0, int(round(6*x/5.-y/5.))):max(int(round(4*x/5.+y/5.)), int(round(6*x/5.-y/5.))+1)].mean() + \
             scores[max(0, int(round(4*y/5.+x/5.))):max(int(round(6*y/5.-x/5.)), int(round(4*y/5.+x/5.))+1)].mean())) \
             for x in starts for y in ends if x+1 < y]
+            import pdb; pdb.set_trace()
         else:
             props = [(0, len(scores), 1, scores.mean())]
         bboxes.extend(props)
