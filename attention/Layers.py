745826167bc3e237ee65517cceb4f640a80f3c33
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-09-03 13:28:54 +0800

    use structure pyramid

--- attention/Layers.py
+++ attention/Layers.py
@@ -89,10 +89,10 @@ class ROI_Relation(nn.Module):
     def __init__(self, d_model, roipool_size, d_inner_hid, n_head, 
                  d_k, d_v, dropout=0.1, kernel_type='roi_remov'):
         super(ROI_Relation, self).__init__()
-        start_pool_size = 1
+        start_pool_size = 2
         self.roi_pool = BRoI1DPool(roipool_size, 1., start_pool_size, start_pool_size, 1./5)
         # self.roi_fc = nn.Linear(d_model*(2*start_pool_size+roipool_size), d_model)
-        self.roi_fc = nn.Sequential(nn.Conv1d(d_model, d_model, 3, padding=0), nn.ReLU(),
+        self.roi_fc = nn.Sequential(nn.Conv1d(d_model, d_model, 3, padding=0, stride=2), nn.ReLU(),
                                     nn.Conv1d(d_model, d_model, 3, padding=0))
 
         self.rank_fc = nn.Linear(d_model, d_model)
