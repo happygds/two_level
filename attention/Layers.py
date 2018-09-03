a9d87fcb7cbda580412dd926689c45039a23b310
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-09-03 19:37:48 +0800

    use structure pyramid

--- attention/Layers.py
+++ attention/Layers.py
@@ -91,9 +91,9 @@ class ROI_Relation(nn.Module):
         super(ROI_Relation, self).__init__()
         start_pool_size = 1
         self.roi_pool = BRoI1DPool(roipool_size, 1., start_pool_size, start_pool_size, 1./5)
-        # self.roi_fc = nn.Linear(d_model*(2*start_pool_size+roipool_size), d_model)
-        self.roi_conv1 = nn.Conv1d(d_model, 2*d_model, 3, padding=0, stride=1)
-        self.roi_conv1 = nn.Conv1d(d_model, 2*d_model, 3, padding=0, stride=1)
+        self.roi_fc = nn.Linear(d_model*(2*start_pool_size+roipool_size), d_model)
+        # self.roi_conv1 = nn.Conv1d(d_model, 2*d_model, 3, padding=0, stride=1)
+        # self.roi_conv1 = nn.Conv1d(d_model, 2*d_model, 3, padding=0, stride=1)
 
         self.rank_fc = nn.Linear(d_model, d_model)
         # for non-local operation
@@ -107,9 +107,9 @@ class ROI_Relation(nn.Module):
         roi_feats = self.roi_pool(features, rois)
         roi_feat_size = roi_feats.size()
         roi_feats = roi_feats.view((-1,)+roi_feat_size[2:])
-        roi_feats = F.glu(self.roi_conv1(roi_feats), dim=1)
-        roi_feats = F.glu(self.roi_conv1(roi_feats), dim=1).view(roi_feat_size[:2]+(-1,))
-        # roi_feats = self.roi_fc(roi_feats.view(roi_feat_size[:2]+(-1,)))
+        # roi_feats = F.glu(self.roi_conv1(roi_feats), dim=1)
+        # roi_feats = F.glu(self.roi_conv1(roi_feats), dim=1).view(roi_feat_size[:2]+(-1,))
+        roi_feats = F.selu(self.roi_fc(roi_feats.view(roi_feat_size[:2]+(-1,))))
 
         # compute mask
         mb_size, len_k = roi_feats.size()[:2]
