7e4f85b39ae32a62f4465845beb134f6e1745f29
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-09-05 15:35:33 +0800

    use multiple thresholds

--- attention/utils.py
+++ attention/utils.py
@@ -83,7 +83,8 @@ class CE_Criterion(nn.Module):
         assert x.size(2) == len(self.thres)
         for i, thre in enumerate(self.thres):
             if self.use_weight:
-                target = torch.gt(y, thre).float()
+                target = torch.gt(y[:, :, 1:], thre).float()
+                target = torch.cat([1.-target, target], dim=2)
                 target *= mask.unsqueeze(2)
                 # cls_weight = 1. / target.mean(0).mean(0)
                 weight = target.sum(1) / mask.sum(1).unsqueeze(1).clamp(eps)
