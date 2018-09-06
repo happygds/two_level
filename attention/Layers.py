5b99ee8833707494c058329b94de35dbfb36512d
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-09-06 15:29:44 +0800

    remove geometry weight in roi_relation module

--- attention/Layers.py
+++ attention/Layers.py
@@ -122,6 +122,6 @@ class ROI_Relation(nn.Module):
 
         enc_output, _ = self.slf_attn(
             enc_output, enc_output, enc_output,
-            attn_mask=rois_attn_mask, attn_pos_emb=rois_pos_emb)
+            attn_mask=rois_attn_mask)
         enc_output = self.pos_ffn(enc_output)
         return enc_output
