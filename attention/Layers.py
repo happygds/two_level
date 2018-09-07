ed102c7b95a190bde419bc3db628e9a43a809eb9
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-09-07 10:04:57 +0800

    implement bsn network

--- attention/Layers.py
+++ attention/Layers.py
@@ -90,7 +90,7 @@ class ROI_Relation(nn.Module):
                  d_k, d_v, dropout=0.1, kernel_type='roi_remov'):
         super(ROI_Relation, self).__init__()
         start_pool_size = 1
-        self.roi_pool = BRoI1DPool(roipool_size, 1., start_pool_size, start_pool_size, 1./10)
+        self.roi_pool = BRoI1DPool(roipool_size, 1., start_pool_size, start_pool_size, 1./5)
         self.roi_fc = nn.Linear(d_model*(2*start_pool_size+roipool_size), d_model)
         # self.roi_conv1 = nn.Conv1d(d_model, 2*d_model, 3, padding=0, stride=1)
         # self.roi_conv1 = nn.Conv1d(d_model, 2*d_model, 3, padding=0, stride=1)
