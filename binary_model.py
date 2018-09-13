d5a65e579ac9e0d12f950470e3b0e297ea52b581
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-09-13 21:43:23 +0800

    use longer sequence

--- binary_model.py
+++ binary_model.py
@@ -94,7 +94,7 @@ class BinaryClassifier(torch.nn.Module):
         rois_pos_emb = pos_embedding(rois_relative_pos, self.d_model)
         roi_feats = self.roi_relations(enc_input, start_rois, end_rois, rois, rois_mask, rois_pos_emb)
         rois_size = rois.size()
-        roi_scores = F.softmax(self.roi_cls(roi_feats).view(size[:2]+(-1, 2)), dim=3)
+        roi_scores = F.softmax(self.roi_cls(roi_feats).view(rois_size[:2]+(-1, 2)), dim=3)
         # import pdb; pdb.set_trace()
 
         if not test_mode:
