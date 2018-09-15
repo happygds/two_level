bf94ed1b148e2a58a6024b1b13b98f15023a6a7d
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-09-15 22:16:35 +0800

    use higher nms threshold

--- attention/proposal.py
+++ attention/proposal.py
@@ -71,7 +71,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
         bboxes = list(filter(lambda b: b[1] - b[0] > 0, bboxes))
         # bboxes.sort(key=lambda x: x[3], reverse=True)
         # bboxes = bboxes[:rpn_post_nms_top]
-        bboxes = temporal_nms(bboxes, 0.9)[:rpn_post_nms_top]
+        bboxes = temporal_nms(bboxes, 0.7)[:rpn_post_nms_top]
         if len(bboxes) == 0:
             bboxes = [(0, len(scores), 1, scores.sum())]
         # import pdb; pdb.set_trace()
