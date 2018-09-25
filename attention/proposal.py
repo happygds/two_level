d6de952f172ea10c0cd8dac523533e23c6131968
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-09-25 17:43:56 +0800

    implement bsn network

--- attention/proposal.py
+++ attention/proposal.py
@@ -114,8 +114,8 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
     rois_start, rois_end = rpn_rois[:, :, 1], rpn_rois[:, :, 2]
     # xx1, yy1 = np.maximum(rois_start[:, np.newaxis, :], rois_start[:, :, np.newaxis]), np.minimum(rois_end[:, np.newaxis, :], rois_end[:, :, np.newaxis])
     # rois_iou = (yy1 - xx1).clip(0.)
-    rois_relative_pos[:, :, :, 0] = 50. * (rois_start[:, np.newaxis, :] - rois_start[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
-    rois_relative_pos[:, :, :, 1] = 50. * (rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
+    rois_relative_pos[:, :, :, 0] = 50. * (rois_start[:, np.newaxis, :] - rois_start[:, :, np.newaxis])
+    rois_relative_pos[:, :, :, 1] = 50. * (rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis])
     # rois_relative_pos[:, :, :, 2] = 20. * np.log((np.abs(rois_cent[:, np.newaxis, :] - rois_cent[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)).clip(1e-3))
     # rois_relative_pos[:, :, :, 3] = 20. * np.log((rois_dura[:, :, np.newaxis] / rois_dura[:, np.newaxis, :].clip(1e-14)).clip(1e-3))
     rois_relative_pos = rois_relative_pos * rpn_rois_mask[:, :, np.newaxis, np.newaxis] * rpn_rois_mask[:, np.newaxis, :, np.newaxis]
