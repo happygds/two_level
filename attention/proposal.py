f3f06388ad0d46400000c3ffad8ba7dd2c1ed7ac
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-09-26 22:24:08 +0800

    implement bsn network

--- attention/proposal.py
+++ attention/proposal.py
@@ -116,8 +116,8 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
     # rois_iou = (yy1 - xx1).clip(0.)
     rois_relative_pos[:, :, :, 0] = 50. * (rois_start[:, np.newaxis, :] - rois_start[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
     rois_relative_pos[:, :, :, 1] = 50. * (rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
-    rois_relative_pos[:, :, :, 2] = 20. * np.log((np.abs(rois_cent[:, np.newaxis, :] - rois_cent[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)).clip(1e-3))
-    rois_relative_pos[:, :, :, 3] = 20. * np.log((rois_dura[:, :, np.newaxis] / rois_dura[:, np.newaxis, :].clip(1e-14)).clip(1e-3))
+    # rois_relative_pos[:, :, :, 2] = 20. * np.log((np.abs(rois_cent[:, np.newaxis, :] - rois_cent[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)).clip(1e-3))
+    # rois_relative_pos[:, :, :, 3] = 20. * np.log((rois_dura[:, :, np.newaxis] / rois_dura[:, np.newaxis, :].clip(1e-14)).clip(1e-3))
     rois_relative_pos = rois_relative_pos * rpn_rois_mask[:, :, np.newaxis, np.newaxis] * rpn_rois_mask[:, np.newaxis, :, np.newaxis]
 
     start_rois = torch.from_numpy(start_rois).cuda().requires_grad_(False).cuda()
