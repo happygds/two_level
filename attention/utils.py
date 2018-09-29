9448d0dad3a883cf8f6fe4b3f788c7a6af3d2281
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-09-29 19:27:21 +0800

    use rank loss

--- attention/utils.py
+++ attention/utils.py
@@ -83,7 +83,7 @@ class Rank_Criterion(nn.Module):
         mask = mask.unsqueeze(1) * mask.unsqueeze(2)
         y_mask = torch.gt(y.unsqueeze(1) - y.unsqueeze(2), 0.).float()
         pred = F.relu(self.epsilon - (x.unsqueeze(1) - x.unsqueeze(2))) * y_mask * mask
-        output = (pred ** 2).sum(2).sum(1) / mask.sum(2).sum(1).clamp(eps)
+        output = (pred ** 2).sum(2).sum(1)
 
         return output.mean()
 
