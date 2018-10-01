3826f62108c0afa9e8dc2b181d9f1008151e43ca
dmlab_gds <dmlab_gds@sjtu.edu.cn>
2018-10-01 10:15:07 +0800

    implement bsn network

--- binary_test.py
+++ binary_test.py
@@ -151,7 +151,7 @@ def process(loader, state_dict, net):
         with torch.no_grad():
             rois, actness, roi_scores = net(feature, pos_ind, feature_mask=feature_mask, test_mode=True)
             rois, actness, roi_scores = rois[0].cpu().numpy(), actness[0].cpu().numpy(), roi_scores[0].cpu().numpy()[:, 1]
-            import pdb; pdb.set_trace()
+            # import pdb; pdb.set_trace()
             outputs = [rois, actness, roi_scores, num_feat]
             result[video_id] = outputs
     return result
