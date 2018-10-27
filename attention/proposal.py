ff65b91212629930ddceada4af257cce5041e6b0
happygds <dmlab_gds@sjtu.edu.cn>
2018-10-27 20:07:06 +0800

    debug AR@100

--- attention/proposal.py
+++ attention/proposal.py
@@ -82,9 +82,9 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
             gt_k, rois = np.asarray(gt_k), np.asarray(rois)
             rois_iou = wrapper_segment_iou(gt_k, rois).max(axis=1)
             pos_bboxes, neg_bboxes = bboxes[rois_iou > 0.7], bboxes[rois_iou < 0.3]
-            rpn_post_nms_top = min(min(len(pos_bboxes) * 3, len(neg_bboxes) * 3 / 2), rpn_post_nms_top)
+            kept_num = min(min(len(pos_bboxes) * 3, len(neg_bboxes) * 3 / 2), rpn_post_nms_top)
             np.random.shuffle(pos_bboxes), np.random.shuffle(neg_bboxes)
-            pos_bboxes, neg_bboxes = pos_bboxes[:int(rpn_post_nms_top/3.)], neg_bboxes[:int(rpn_post_nms_top*2./3.)]
+            pos_bboxes, neg_bboxes = pos_bboxes[:int(kept_num/3.)], neg_bboxes[:int(kept_num*2./3.)]
             bboxes = np.concatenate([pos_bboxes, neg_bboxes], axis=0)
             np.random.shuffle(bboxes)
         bboxes = bboxes[:rpn_post_nms_top]
@@ -124,7 +124,6 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
     rois_start, rois_end = rpn_rois[:, :, 1], rpn_rois[:, :, 2]
     # xx1, yy1 = np.maximum(rois_start[:, np.newaxis, :], rois_start[:, :, np.newaxis]), np.minimum(rois_end[:, np.newaxis, :], rois_end[:, :, np.newaxis])
     # rois_iou = (yy1 - xx1).clip(0.)
-    import pdb; pdb.set_trace()
     rois_relative_pos[:, :, :, 0] = 20. * (rois_start[:, np.newaxis, :] - rois_start[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
     rois_relative_pos[:, :, :, 1] = 20. * (rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
     # rois_relative_pos[:, :, :, 1] = 20. * np.log((np.abs(rois_cent[:, np.newaxis, :] - rois_cent[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)).clip(1e-3))
