6ad877cf7eb36d67d091c38edf4ab43862a8fbab
happygds <dmlab_gds@sjtu.edu.cn>
2018-10-30 11:55:06 +0800

    replace nms with softnms

--- load_binary_score.py
+++ load_binary_score.py
@@ -251,7 +251,7 @@ class BinaryDataSet(data.Dataset):
             assert begin_ind == 0, "{} != 0".format(begin_ind)
             gts[:len(video.gts)] = (min_len - video.gts)[:, ::-1].copy()
             out_feat, out_label = out_feat[::-1].copy(), out_label[::-1].copy()
-            out_starts, out_ends = out_starts[::-1].copy(), out_ends[::-1].copy()
+            out_starts, out_ends = out_ends[::-1].copy(), out_starts[::-1].copy()
 
         pos_ind = torch.from_numpy(np.arange(begin_ind, end_ind)).long()
         out_feat = torch.from_numpy(out_feat)
