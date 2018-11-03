2f70e8aa7618f094bcf89d6a0200ea6c4636ac18
happygds <dmlab_gds@sjtu.edu.cn>
2018-11-03 20:00:09 +0800

    softnms

--- submit_test.py
+++ submit_test.py
@@ -244,5 +244,5 @@ for _, vid_name in enumerate(vid_names):
     results[vid_name] = this_lst
 submit_pred['results'] = results
 
-with open('/data1/matheguo/important/{}.json'.format('submit_test'), 'w') as outfile:
+with open('{}.json'.format('submit_test'), 'w') as outfile:
     json.dump(submit_pred, outfile, indent=4, separators=(',', ': '))
