2090361f766da3f4dbfd8eb3eb7c5f990768abcb
happygds <dmlab_gds@sjtu.edu.cn>
2018-11-05 20:07:38 +0800

    implement detection

--- ssn_opts.py
+++ ssn_opts.py
@@ -13,9 +13,8 @@ parser.add_argument('--model', default='TAG', type=str,
 parser.add_argument('--feat_model', default='i3d_rgb', type=str,
                     help='the model for extracting pretrained features ('
                     'i3d_rgb | i3d_rgb_trained | inception_resnet_v2 | inception_resnet_v2_trained)')
-parser.add_argument('--use_flow', action='store_true',
-                    help='whether use i3d_flow feature')
-parser.set_defaults(use_flow=True)    # for self-attetion encoder
+parser.add_argument('--use_flow', default=True, type=int,
+                    help='whether use i3d_flow feature') # for self-attetion encoder
 parser.add_argument('--pos_enc', default=False, type=int,
                     help='whether slice the original position indices of the input video sequence')
 parser.add_argument('--att_kernel_type', default='self_attn',
