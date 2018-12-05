07a1b8f2ec871d28abd732e371a6bb24818ee0e7
happygds <dmlab_gds@sjtu.edu.cn>
2018-12-05 21:54:14 +0800

    debug

--- binary_model.py
+++ binary_model.py
@@ -8,6 +8,7 @@ from attention.Layers import EncoderLayer, Local_EncoderLayer, ROI_Relation
 from attention.proposal import proposal_layer
 from attention.utils import *
 
+
 class BinaryClassifier(torch.nn.Module):
     def __init__(self, num_class, course_segment, args, dropout=0.1, test_mode=False):
 
@@ -26,14 +27,14 @@ class BinaryClassifier(torch.nn.Module):
         if args.num_local > 0:
             self.layer_stack = nn.ModuleList([
                 Local_EncoderLayer(args.d_model, args.d_inner_hid, args.n_head, args.d_k,
-                                   args.d_v, dropout=0.1, kernel_type=args.att_kernel_type, 
+                                   args.d_v, dropout=0.1, kernel_type=args.att_kernel_type,
                                    local_type=args.local_type)
                 for _ in range(args.n_layers)])
         else:
             self.layer_stack = nn.ModuleList([
                 EncoderLayer(args.d_model, args.d_inner_hid, args.n_head, args.d_k,
-                            args.d_v, dropout=0.1, kernel_type=args.att_kernel_type, 
-                            groupwise_heads=args.groupwise_heads)
+                             args.d_v, dropout=0.1, kernel_type=args.att_kernel_type,
+                             groupwise_heads=args.groupwise_heads)
                 for _ in range(args.n_layers)])
 
         self.d_model = args.d_model
@@ -44,12 +45,12 @@ class BinaryClassifier(torch.nn.Module):
         self.dilated_mask = args.dilated_mask
         self.trn_kernel = args.groupwise_heads
 
-        self.roi_relations = ROI_Relation(args.d_model, args.roi_poolsize, args.d_inner_hid, 
+        self.roi_relations = ROI_Relation(args.d_model, args.roi_poolsize, args.d_inner_hid,
                                           args.n_head, args.d_k, args.d_v, dropout=0.1)
         self.roi_cls = nn.Linear(args.d_model, 2)
 
-    def forward(self, feature, pos_ind, target=None, gts=None, 
-                feature_mask=None, test_mode=False, ensemble_stage=None, 
+    def forward(self, feature, pos_ind, target=None, gts=None,
+                feature_mask=None, test_mode=False, ensemble_stage=None,
                 score_output_before=None):
         # Word embedding look up
         if self.reduce:
@@ -62,13 +63,17 @@ class BinaryClassifier(torch.nn.Module):
             enc_slf_attn_mask = (
                 1. - feature_mask).unsqueeze(1).expand(mb_size, len_k, len_k).byte()
         else:
-            enc_slf_attn_mask = torch.zeros((mb_size, len_k, len_k)).byte().cuda()
+            enc_slf_attn_mask = torch.zeros(
+                (mb_size, len_k, len_k)).byte().cuda()
         local_attn_mask = None
         if self.num_local > 0:
-            local_attn_mask = get_attn_local_mask(enc_slf_attn_mask, num_local=self.num_local)
+            local_attn_mask = get_attn_local_mask(
+                enc_slf_attn_mask, num_local=self.num_local)
             if self.dilated_mask:
-                enc_slf_attn_mask = get_attn_dilated_mask(enc_slf_attn_mask, num_local=self.num_local)
-        enc_slf_attn_mask = torch.gt(enc_slf_attn_mask + enc_slf_attn_mask.transpose(1, 2), 0)
+                enc_slf_attn_mask = get_attn_dilated_mask(
+                    enc_slf_attn_mask, num_local=self.num_local)
+        enc_slf_attn_mask = torch.gt(
+            enc_slf_attn_mask + enc_slf_attn_mask.transpose(1, 2), 0)
 
         size = enc_input.size()
         enc_output = enc_input
@@ -79,10 +84,10 @@ class BinaryClassifier(torch.nn.Module):
         else:
             slf_local_mask = None
 
-        if score_output_before is None:      
+        if score_output_before is None:
             for i, enc_layer in enumerate(self.layer_stack):
                 enc_output, enc_slf_attn = enc_layer(
-                    enc_output, local_attn_mask=slf_local_mask, 
+                    enc_output, local_attn_mask=slf_local_mask,
                     slf_attn_mask=slf_attn_mask)
             score_output_before = self.scores(enc_output)
         if ensemble_stage == '1':
@@ -93,13 +98,16 @@ class BinaryClassifier(torch.nn.Module):
 
         # compute loss for training/validation stage
         if not test_mode:
-            start_rois, end_rois, rois, rois_mask, rois_relative_pos, labels = proposal_layer(score_output, feature_mask, gts=gts, test_mode=test_mode)
+            start_rois, end_rois, rois, rois_mask, rois_relative_pos, labels = proposal_layer(
+                score_output, feature_mask, gts=gts, test_mode=test_mode)
         else:
-            start_rois, end_rois, rois, rois_mask, rois_relative_pos, actness = proposal_layer(score_output, feature_mask, test_mode=test_mode)
+            start_rois, end_rois, rois, rois_mask, rois_relative_pos, actness = proposal_layer(
+                score_output, feature_mask, test_mode=test_mode)
 
         # use relative position embedding
         rois_pos_emb = pos_embedding(rois_relative_pos, self.d_model)
-        roi_feats = self.roi_relations(enc_input, start_rois, end_rois, rois, rois_mask, rois_pos_emb)
+        roi_feats = self.roi_relations(
+            enc_input, start_rois, end_rois, rois, rois_mask, rois_pos_emb)
         rois_size = rois.size()
         roi_scores_before = self.roi_cls(roi_feats)
         roi_scores = F.softmax(roi_scores_before, dim=2)
