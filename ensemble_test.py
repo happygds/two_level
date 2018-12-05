d850ed02eb07ef3c82d927202143452901230fc8
happygds <dmlab_gds@sjtu.edu.cn>
2018-12-05 09:47:10 +0800

    debug

--- ensemble_test.py
+++ ensemble_test.py
@@ -199,69 +199,60 @@ if __name__ == '__main__':
             w.daemon = True
             w.start()
 
-        out_dict = {}
+        out_stage1 = {}
         for i in range(max_num):
             rst = result_queue.get()
-            out_dict[rst[0]] = rst[1]
-        ensemble_outputs[model_id] = out_dict
+            out_stage1[rst[0]] = rst[1]
         for w in workers:
             w.terminate()
 
-    stage1_outs = {}
-    for key in out_dict.keys():
-        for model_id in range(1, args.num_ensemble+1, 1):
-            if model_id == 1:
-                this_mean = ensemble_outputs[model_id][key] / args.num_ensemble
-            else:
-                this_mean += ensemble_outputs[model_id][key] / args.num_ensemble
-        stage1_outs[key] = this_mean
+        ensemble_stage2 = {}
+        # stage 2 : suppose ensemble models from seed1-seedN
+        for stage2_id in range(1, args.num_ensemble+1, 1):
+            this_path = (args.weights + '.')[:-1]
+            this_path = this_path.replace("seed1", "seed"+str(stage2_id))
+            ctx = multiprocessing.get_context('spawn')
+            checkpoint = torch.load(this_path)
 
-    # suppose ensemble models from seed1-seedN
-    ensemble_outputs = {}
-    for model_id in range(1, args.num_ensemble+1, 1):
-        this_path = (args.weights + '.')[:-1]
-        this_path = this_path.replace("seed1", "seed"+str(model_id))
-        ctx = multiprocessing.get_context('spawn')
-        checkpoint = torch.load(this_path)
+            print("model epoch {} loss: {}".format(
+                checkpoint['epoch'], checkpoint['best_loss']))
+            base_dict = {'.'.join(k.split('.')[1:]): v for k, v in list(
+                checkpoint['state_dict'].items())}
 
-        print("model epoch {} loss: {}".format(
-            checkpoint['epoch'], checkpoint['best_loss']))
-        base_dict = {'.'.join(k.split('.')[1:]): v for k, v in list(
-            checkpoint['state_dict'].items())}
+            index_queue = ctx.Queue()
+            result_queue = ctx.Queue()
+            workers = [ctx.Process(target=runner_func, args=(
+                dataset, base_dict, gpu_list[i % len(gpu_list)], index_queue, result_queue, "2", out_stage1))
+                for i in range(args.workers)]
 
-        index_queue = ctx.Queue()
-        result_queue = ctx.Queue()
-        workers = [ctx.Process(target=runner_func, args=(
-            dataset, base_dict, gpu_list[i % len(gpu_list)], index_queue, result_queue, "2", stage1_outs))
-            for i in range(args.workers)]
+            max_num = args.max_num if args.max_num > 0 else len(dataset)
 
-        max_num = args.max_num if args.max_num > 0 else len(dataset)
+            for i in range(max_num):
+                index_queue.put(i)
 
-        for i in range(max_num):
-            index_queue.put(i)
+            for w in workers:
+                w.daemon = True
+                w.start()
 
-        for w in workers:
-            w.daemon = True
-            w.start()
+            out_stage2 = {}
+            for i in range(max_num):
+                rst = result_queue.get()
+                out_stage2[rst[0]] = rst[1]
+            ensemble_stage2[stage2_id] = out_stage2
+            for w in workers:
+                w.terminate()
 
-        out_dict = {}
-        for i in range(max_num):
-            rst = result_queue.get()
-            out_dict[rst[0]] = rst[1]
-        ensemble_outputs[model_id] = out_dict
-        for w in workers:
-            w.terminate()
-
-    stage2_outs = {}
-    for key in out_dict.keys():
-        for model_id in range(1, args.num_ensemble+1, 1):
-            if model_id == 1:
-                this_mean = ensemble_outputs[model_id][key][2] / args.num_ensemble
-            else:
-                this_mean += ensemble_outputs[model_id][key][2] / args.num_ensemble
-        import pdb; pdb.set_trace()
-        this_mean = np_softmax(this_mean)[:, 1]
-        stage2_outs[key] = ensemble_outputs[model_id][key][:2] + [this_mean,] + ensemble_outputs[model_id][key][3:]
+        stage2_outs = {}
+        for key in out_stage2.keys():
+            for stage2_id in range(1, args.num_ensemble+1, 1):
+                if stage2_id == 1:
+                    this_mean = ensemble_stage2[stage2_id][key][2] / args.num_ensemble
+                else:
+                    this_mean += ensemble_stage2[stage2_id][key][2] / args.num_ensemble
+            this_mean = np_softmax(this_mean)[:, 1]
+            stage2_outs[key] = ensemble_stage2[stage2_id][key][:2] + [this_mean,] + ensemble_stage2[stage2_id][key][3:]
+        
+        ensemble_outputs[model_id] = stage2_outs
 
     if args.save_scores is not None:
         out_dict = stage2_outs
