5b363fa77fc74df7b80d8caff3b5c3a5c964bfbc
happygds <dmlab_gds@sjtu.edu.cn>
2018-12-10 14:33:01 +0800

    debug ensemble

--- attention/proposal.py
+++ attention/proposal.py
@@ -72,7 +72,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
         if epoch_id is not None and epoch_id < 3:
             bboxes = temporal_nms(bboxes, 0.9)[:rpn_post_nms_top]
         else:
-            bboxes = Soft_NMS(bboxes, length=len(scores))[:rpn_post_nms_top]
+            bboxes = Soft_NMS(bboxes, length=len(scores), max_num=rpn_post_nms_top+1)[:rpn_post_nms_top]
         if len(bboxes) == 0:
             bboxes = [(0, len(scores)-1, 1, scores.mean()*pstarts[0]*pends[-1])]
 
