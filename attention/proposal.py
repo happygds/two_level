82b8b97c5c52b3904fecc4c09d3c17764e18b86a
happygds <dmlab_gds@sjtu.edu.cn>
2018-12-12 22:30:03 +0800

    rescale to use original length

--- attention/proposal.py
+++ attention/proposal.py
@@ -105,7 +105,8 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
             gt_k = gts[k]
             gt_k = [x.cpu().numpy() for x in gt_k]
             gt_k = list(filter(lambda b: b[1] + b[0] > 0, gt_k))
-            pool.apply_async(gen_prop, args=(k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id), callback=call_back)
+            # pool.apply_async(gen_prop, args=(k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id), callback=call_back)
+            _, bboxes_dict[k], rois_iou_dict[k] = gen_prop(k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id)
         pool.close()
         pool.join()
         del pool
