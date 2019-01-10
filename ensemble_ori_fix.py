42689f6dc9f08cad6ceaad2bc14ea3411912eeca
happygds <dmlab_gds@sjtu.edu.cn>
2019-01-10 21:41:44 +0800

    remove 100 restriction

--- ensemble_ori_fix.py
+++ ensemble_ori_fix.py
@@ -133,8 +133,8 @@ def compute_frame_count(video_info, frame_path, name_pattern):
     # first count frame numbers
     try:
         video_name = video_info.id
-        # import pdb; pdb.set_trace()
         path = os.path.join(frame_path, video_name, name_pattern)
+        import pdb; pdb.set_trace()
         files = glob.glob(path)
         frame_cnt = len(files)
     except:
