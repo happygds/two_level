1b0f2a6d79ae6667198e212d2a90e542bbbd6830
happygds <dmlab_gds@sjtu.edu.cn>
2019-01-11 11:16:08 +0800

    remove 100 restriction

--- attention/proposal.py
+++ attention/proposal.py
@@ -37,6 +37,7 @@ def gen_prop(x):
     bboxes = temporal_nms(bboxes, 1.0 - 1e-14)
     # bboxes = bboxes[:rpn_post_nms_top]
     num_keep = int(round(0.125*len(bboxes)))
+    num_keep = min(max(num_keep, rpn_post_nms_top//2), rpn_post_nms_top)
     if epoch_id is not None and epoch_id < 0:
         bboxes = temporal_nms(bboxes, 0.9)[:num_keep]
     else:
