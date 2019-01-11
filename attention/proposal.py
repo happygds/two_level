cdade89da79b8071e8286f5fb9c02d973287df52
happygds <dmlab_gds@sjtu.edu.cn>
2019-01-11 13:27:01 +0800

    remove 100 restriction

--- attention/proposal.py
+++ attention/proposal.py
@@ -69,7 +69,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False,
             bboxes.extend(props)
             bboxes = list(filter(lambda b: b[1] - b[0] > 0, bboxes))
             # to remove duplicate proposals
-            import pdb; pdb.set_trace()
+            # import pdb; pdb.set_trace()
             bboxes = temporal_nms(bboxes, 1.0 - 1e-14)
             # bboxes = Soft_NMS(bboxes, length=len(scores), max_num=rpn_post_nms_top)[:rpn_post_nms_top]
             if ensemble_stage == '1':
