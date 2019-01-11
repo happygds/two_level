5784dcd5b950257b232759c623b72acf96764fcc
happygds <dmlab_gds@sjtu.edu.cn>
2019-01-11 11:31:29 +0800

    remove 100 restriction

--- attention/proposal.py
+++ attention/proposal.py
@@ -59,7 +59,7 @@ def gen_prop(x):
 
 
 def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_prob=0., 
-                   rpn_post_nms_top=160, feat_stride=16, epoch_id=None):
+                   rpn_post_nms_top=128, feat_stride=16, epoch_id=None):
     """
     Parameters
     ----------
@@ -82,6 +82,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
 
     if test_mode:
         assert len(feature_mask) == 1
+        rpn_post_nms_top = 160
         actness = np.zeros((batch_size, rpn_post_nms_top))
     rpn_rois = np.zeros((batch_size, rpn_post_nms_top, 3))
     start_rois, end_rois = np.zeros_like(rpn_rois), np.zeros_like(rpn_rois)
