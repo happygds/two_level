cc0e637ebd7afd122636f88a25d86d1ecbe8627f
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-01 15:49:28 +0800

    decode video to images

--- attention/proposal.py
+++ attention/proposal.py
@@ -111,7 +111,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
             num_feat = int(feature_mask[k].sum())
             scores_k = score_output[k][:num_feat]
             gt_k = gts[k]
-            gt_k = [x for x in gt_k]
+            # gt_k = [x.cpu().numpy() for x in gt_k]
             gt_k = list(filter(lambda b: b[1] + b[0] > 0, gt_k))
             sample_infos.append([k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id])
             # _, bboxes_dict[k], rois_iou_dict[k] = gen_prop([k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id])
