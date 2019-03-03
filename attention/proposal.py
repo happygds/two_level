a4a5ddb37d811fb868eba8b9e5ed6d8a5cc8990d
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-03 21:56:30 +0800

    use 4 position embedding

--- attention/proposal.py
+++ attention/proposal.py
@@ -110,15 +110,15 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
             gt_k = gts[k]
             gt_k = [x.cpu().numpy() for x in gt_k]
             gt_k = list(filter(lambda b: b[1] + b[0] > 0, gt_k))
-            if epoch_id is not None:
-                sample_infos.append([k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id])
-            else:
-                _, bboxes_dict[k], rois_iou_dict[k] = gen_prop([k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id])
-        if epoch_id is not None:
-            pool = mp.Pool(processes=16)
-            handle=[pool.apply_async(gen_prop, args=(x,), callback=call_back) for x in sample_infos]
-            pool.close()
-            pool.join()
+            # if epoch_id is not None:
+            #     sample_infos.append([k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id])
+            # else:
+            _, bboxes_dict[k], rois_iou_dict[k] = gen_prop([k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id])
+        # if epoch_id is not None:
+        #     pool = mp.Pool(processes=16)
+        #     handle=[pool.apply_async(gen_prop, args=(x,), callback=call_back) for x in sample_infos]
+        #     pool.close()
+        #     pool.join()
 
     # if test_mode:
     #     assert batch_size == 1
