a9d0103f98fc41b46526a924d67aee4fd3f6d4d6
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-03 18:06:44 +0800

    overlap > 0

--- attention/proposal.py
+++ attention/proposal.py
@@ -160,7 +160,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
     # rois_relative_pos[:, :, :, 2] = 1. * (rois_start[:, np.newaxis, :] - rois_start[:, :, np.newaxis]) / rois_dura[:, :, np.newaxis].clip(1e-14)
     # rois_relative_pos[:, :, :, 3] = 1. * (rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis]) / rois_dura[:, :, np.newaxis].clip(1e-14)
     rois_relative_pos = rois_relative_pos * rpn_rois_mask[:, :, np.newaxis, np.newaxis] * rpn_rois_mask[:, np.newaxis, :, np.newaxis]
-    # import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
 
     start_rois = torch.from_numpy(start_rois).cuda().requires_grad_(False).cuda()
     end_rois = torch.from_numpy(end_rois).cuda().requires_grad_(False).cuda()
