6ed56671c7f94e13005755e41478a552a3b776e5
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-05 16:35:25 +0800

    dropout rate

--- attention/proposal.py
+++ attention/proposal.py
@@ -41,7 +41,6 @@ def gen_prop(x):
     # num_keep = min(max(num_keep, rpn_post_nms_top//2), rpn_post_nms_top)
     num_keep = rpn_post_nms_top
     # if epoch_id is not None and epoch_id < 3:
-    bboxes = []
     bboxes = temporal_nms(bboxes, 0.9)[:num_keep]
     # else:
     # bboxes = Soft_NMS(bboxes, length=len(scores), max_num=num_keep)
