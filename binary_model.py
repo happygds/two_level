17a192c58a066445f0cfddb475690c2166f7579e
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-05 12:25:54 +0800

    dropout rate

--- binary_model.py
+++ binary_model.py
@@ -19,7 +19,7 @@ class BinaryClassifier(torch.nn.Module):
         self.reduce = args.reduce_dim > 0
         if self.reduce:
             self.reduce_layer = nn.Sequential(
-                nn.Linear(args.input_dim, args.reduce_dim), nn.SELU(), nn.Dropout(self.dropout))
+                nn.Linear(args.input_dim, args.reduce_dim), nn.SELU())
         self.n_layers = args.n_layers
 
         if args.num_local > 0:
