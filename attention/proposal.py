63def26084f9bc69e4c27d1afada4f926e02d79a
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-07 14:49:26 +0800

    debug

--- attention/proposal.py
+++ attention/proposal.py
@@ -95,7 +95,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
         actness = np.zeros((batch_size, rpn_post_nms_top))
     rpn_rois = np.zeros((batch_size, rpn_post_nms_top, 3))
     start_rois, end_rois = np.zeros_like(rpn_rois), np.zeros_like(rpn_rois)
-    labels = np.zeros((batch_size, rpn_post_nms_top, 2))
+    labels = np.zeros((batch_size, rpn_post_nms_top, 4))
 
     # global bboxes_dict, rois_iou_dict
     bboxes_dict, rois_iou_dict = {}, {}
@@ -173,11 +173,16 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
     # xx1, yy1 = np.maximum(start_rois[:, np.newaxis, :, 1] , start_rois[:, :, np.newaxis, 1]), np.minimum(end_rois[:, np.newaxis, :, 2], end_rois[:, :, np.newaxis, 2])
     # rois_iou = (yy1 - xx1).clip(0.)
     # rois_dura_avg = 0.5 * (rois_dura[:, np.newaxis, :] + rois_dura[:, :, np.newaxis])
-    rois_relative_pos[:, :, :, 0] = 10. * (rois_start[:, np.newaxis, :] - rois_start[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
-    rois_relative_pos[:, :, :, 1] = 10. * (rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
-    # rois_relative_pos[:, :, :, 0] = np.log((np.abs(rois_cent[:, np.newaxis, :] - rois_cent[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-3)).clip(1e-3))
-    # rois_relative_pos[:, :, :, 1] = np.log((rois_dura[:, :, np.newaxis] / rois_dura[:, np.newaxis, :].clip(1e-3)).clip(1e-3))
-    # rois_relative_pos[:, :, :, 2] = np.sign(rois_cent[:, np.newaxis, :] - rois_cent[:, :, np.newaxis])
+    # rois_relative_pos[:, :, :, 0] = 10. * (rois_start[:, np.newaxis, :] - rois_start[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
+    # rois_relative_pos[:, :, :, 1] = 10. * (rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
+    rois_relative_pos[:, :, :, 0] = np.log((np.abs(
+        rois_start[:, np.newaxis, :] - rois_start[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-3)).clip(1e-3))
+    rois_relative_pos[:, :, :, 1] = np.sign(
+        rois_start[:, np.newaxis, :] - rois_start[:, :, np.newaxis])
+    rois_relative_pos[:, :, :, 2] = np.log((np.abs(
+        rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-3)).clip(1e-3))
+    rois_relative_pos[:, :, :, 3] = np.sign(
+        rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis])
     rois_relative_pos = rois_relative_pos * \
         rpn_rois_mask[:, :, np.newaxis, np.newaxis] * \
         rpn_rois_mask[:, np.newaxis, :, np.newaxis]
