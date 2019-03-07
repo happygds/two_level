c3a7bf1de76ba7e6d43db111ff01c95b77207e55
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-07 13:36:07 +0800

    debug

--- attention/proposal.py
+++ attention/proposal.py
@@ -174,7 +174,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
     # rois_iou = (yy1 - xx1).clip(0.)
     # rois_dura_avg = 0.5 * (rois_dura[:, np.newaxis, :] + rois_dura[:, :, np.newaxis])
     rois_relative_pos[:, :, :, 0] = (rois_start[:, np.newaxis, :] - rois_start[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
-    rois_relative_pos[:, :, :, 1] = (rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14))
+    rois_relative_pos[:, :, :, 1] = (rois_end[:, np.newaxis, :] - rois_end[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
     # rois_relative_pos[:, :, :, 0] = np.log((np.abs(rois_cent[:, np.newaxis, :] - rois_cent[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-3)).clip(1e-3))
     # rois_relative_pos[:, :, :, 1] = np.log((rois_dura[:, :, np.newaxis] / rois_dura[:, np.newaxis, :].clip(1e-3)).clip(1e-3))
     # rois_relative_pos[:, :, :, 2] = np.sign(rois_cent[:, np.newaxis, :] - rois_cent[:, :, np.newaxis])
