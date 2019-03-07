23bd92ff742a6d439121c35ed651e026cb51fd94
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-07 17:56:45 +0800

    debug

--- attention/proposal.py
+++ attention/proposal.py
@@ -164,7 +164,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
     rpn_rois_mask = (np.abs(rpn_rois[:, :, 1:]).mean(
         axis=2) > 0.).astype('float32')
     rois_relative_pos = np.zeros(
-        (batch_size, rpn_post_nms_top, rpn_post_nms_top, 4)).astype('float32')
+        (batch_size, rpn_post_nms_top, rpn_post_nms_top, 2)).astype('float32')
     # compute geometric attention
     rois_cent, rois_dura = rpn_rois[:, :, 1:].mean(
         axis=2), rpn_rois[:, :, 2] - rpn_rois[:, :, 1]
