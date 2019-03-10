2246226eaac4b99807a664374112cff60decf50b
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-10 23:54:28 +0800

    use instancenorm1d

--- attention/utils.py
+++ attention/utils.py
@@ -87,9 +87,9 @@ class Rank_Criterion(nn.Module):
         mask = mask.unsqueeze(1) * mask.unsqueeze(2)
         y_mask = torch.gt(y.unsqueeze(1) - y.unsqueeze(2), 0.).float()
         
-        # pred = F.relu((y.unsqueeze(1) - y.unsqueeze(2)) - (x.unsqueeze(1) - x.unsqueeze(2))) * y_mask * mask
-        pred = F.relu(self.epsilon - (x.unsqueeze(1) - x.unsqueeze(2))) * y_mask * mask
-        output = (pred).sum(2).sum(1)
+        pred = F.relu((y.unsqueeze(1) - y.unsqueeze(2)) - (x.unsqueeze(1) - x.unsqueeze(2))) * y_mask * mask
+        # pred = F.relu(self.epsilon - (x.unsqueeze(1) - x.unsqueeze(2))) * y_mask * mask
+        output = (pred**2).sum(2).sum(1)
         # output = torch.exp( - (x.unsqueeze(1) - x.unsqueeze(2))) * F.relu(y.unsqueeze(1) - y.unsqueeze(2)) * mask
         # output = torch.abs(output).sum(2).sum(1) / (y_mask * mask).sum(2).sum(1).clamp(eps)
 
