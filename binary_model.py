31ec3180da891b76006177368d9d6adf7fbad4a6
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-11 10:04:18 +0800

    use instancenorm1d

--- binary_model.py
+++ binary_model.py
@@ -46,7 +46,7 @@ class BinaryClassifier(torch.nn.Module):
         self.roi_relations = ROI_Relation(args.d_model, args.roi_poolsize, args.d_inner_hid, 
                                           args.n_head, args.d_k, args.d_v, dropout=0.1)
         self.batchnorm = nn.BatchNorm1d(args.d_model)
-        self.roi_cls = nn.Linear(args.d_model, 2)
+        self.roi_cls = nn.Linear(args.d_model, 1)
 
     def forward(self, feature, pos_ind, target=None, gts=None, 
                 feature_mask=None, test_mode=False, epoch_id=None):
@@ -96,7 +96,7 @@ class BinaryClassifier(torch.nn.Module):
         rois_pos_emb = pos_embedding(rois_relative_pos, self.d_model)
         roi_feats = self.roi_relations(enc_input, start_rois, end_rois, rois, rois_mask, rois_pos_emb)
         roi_feats = self.batchnorm(roi_feats.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
-        roi_scores = F.softmax(self.roi_cls(roi_feats), dim=2)
+        roi_scores = F.sigmoid(self.roi_cls(roi_feats))
         # import pdb; pdb.set_trace()
 
         if not test_mode:
