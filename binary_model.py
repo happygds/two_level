7ab38aba90fd231d8f4ee21b64bd924d6f79774e
happygds <dmlab_gds@sjtu.edu.cn>
2019-03-13 16:12:03 +0800

    debug

--- binary_model.py
+++ binary_model.py
@@ -20,7 +20,7 @@ class BinaryClassifier(torch.nn.Module):
         if self.reduce:
             self.reduce_layer = nn.Sequential(
                 nn.Linear(args.input_dim, args.reduce_dim), nn.SELU(), nn.Dropout(self.dropout))
-            self.inputnorm = nn.BatchNorm1d(args.d_model)
+            # self.inputnorm = nn.BatchNorm1d(args.d_model)
         self.n_layers = args.n_layers
 
         if args.num_local > 0:
@@ -45,7 +45,7 @@ class BinaryClassifier(torch.nn.Module):
 
         self.roi_relations = ROI_Relation(args.d_model, args.roi_poolsize, args.d_inner_hid, 
                                           args.n_head, args.d_k, args.d_v, dropout=self.dropout)
-        self.batchnorm = nn.BatchNorm1d(args.d_model)
+        # self.batchnorm = nn.BatchNorm1d(args.d_model)
         self.roi_cls = nn.Sequential(nn.Dropout(self.dropout), nn.Linear(args.d_model, 2))
 
     def forward(self, feature, pos_ind, target=None, gts=None, 
@@ -53,7 +53,7 @@ class BinaryClassifier(torch.nn.Module):
         # Word embedding look up
         if self.reduce:
             enc_input = self.reduce_layer(feature)
-            enc_input = self.inputnorm(enc_input.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
+            # enc_input = self.inputnorm(enc_input.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
         else:
             enc_input = feature
 
@@ -96,7 +96,7 @@ class BinaryClassifier(torch.nn.Module):
         # use relative position embedding
         rois_pos_emb = pos_embedding(rois_relative_pos, self.d_model)
         roi_feats = self.roi_relations(enc_input, start_rois, end_rois, rois, rois_mask, rois_pos_emb)
-        roi_feats = self.batchnorm(roi_feats.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
+        # roi_feats = self.batchnorm(roi_feats.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
         roi_scores = F.softmax(self.roi_cls(roi_feats), dim=2)
 
         if not test_mode:
