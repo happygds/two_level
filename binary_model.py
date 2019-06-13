50a26616cd879630dd00acca09b96c72d1ce0acd
happygds <dmlab_gds@sjtu.edu.cn>
2019-06-13 08:36:58 +0800

    sigmoid output

--- binary_model.py
+++ binary_model.py
@@ -97,7 +97,7 @@ class BinaryClassifier(torch.nn.Module):
         rois_pos_emb = pos_embedding(rois_relative_pos, self.d_model)
         roi_feats = self.roi_relations(enc_input, start_rois, end_rois, rois, rois_mask, rois_pos_emb)
         # roi_feats = self.batchnorm(roi_feats.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
-        roi_scores = F.sigmoid(self.roi_cls(roi_feats)).unsqueeze(2)
+        roi_scores = F.sigmoid(self.roi_cls(roi_feats)).squeeze(2)
 
         if not test_mode:
             return score_output, enc_slf_attn, roi_scores, labels, rois_mask
