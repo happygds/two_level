61bb86b9f585f2f9b04971be6f3ac6675e686832
happygds <dmlab_gds@sjtu.edu.cn>
2019-06-20 13:45:46 +0800

    revise fcn_tap.tex

--- binary_model.py
+++ binary_model.py
@@ -47,12 +47,12 @@ class BinaryClassifier(torch.nn.Module):
 
         self.roi_relations = ROI_Relation(args.d_model, args.roi_poolsize, args.d_inner_hid,
                                           args.n_head, args.d_k, args.d_v, dropout=self.dropout)
-        self.norm = nn.InstanceNorm1d(args.d_model)
+        self.norm = nn.BatchNorm1d(args.d_model)
         # self.roi_feat_max = nn.Sequential(
         #     nn.Linear(args.d_model, args.d_model), nn.SELU(), nn.Dropout(self.dropout))
         # self.w_roi = nn.Parameter(torch.FloatTensor(1, 1, args.d_model))
         # init.xavier_normal_(self.w_roi)
-        self.roi_cls = nn.Linear(args.d_model, 2)
+        self.roi_cls1 = nn.Linear(args.d_model, 1)
 
     def forward(self, feature, pos_ind, target=None, gts=None,
                 feature_mask=None, test_mode=False, epoch_id=None):
@@ -111,7 +111,7 @@ class BinaryClassifier(torch.nn.Module):
         roi_feats = self.norm(roi_feats.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
         # roi_feat_max = self.roi_feat_max(roi_feats).max(1)[0].unsqueeze(1)
         # roi_feat_max = self.w_roi
-        roi_scores = torch.softmax(self.roi_cls(roi_feats), dim=2)[:, :, 1]
+        roi_scores = torch.sigmoid(self.roi_cls1(roi_feats))[:, :, 0]
         # roi_scores = ((roi_feat_max * roi_feats).sum(2) / torch.sqrt(
         #     (roi_feat_max ** 2).sum(2) * (roi_feats ** 2).sum(2)).clamp(1e-14)).clamp(0.)
 
