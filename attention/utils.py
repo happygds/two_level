c9b409b5907dfcc32e2e3e886985cd78cd1949a8
happygds <dmlab_gds@sjtu.edu.cn>
2019-06-21 19:41:59 +0800

    revise fcn_tap.tex

--- attention/utils.py
+++ attention/utils.py
@@ -95,7 +95,7 @@ class Rank_Criterion(nn.Module):
 
         pred = F.relu(self.epsilon - (x.unsqueeze(1) - x.unsqueeze(2))) * y_mask * mask
         # pred = (F.relu(self.epsilon - (x.unsqueeze(1) - x.unsqueeze(2)) )) ** 2 * y_mask * mask
-        output = pred.sum(2).sum(1) / (y_mask*mask).sum(2).sum(1).clamp(eps)
+        output = pred.sum(2).sum(1) / (y_mask * mask).sum(2).sum(1).clamp(eps)
 
         return output.mean()
 
