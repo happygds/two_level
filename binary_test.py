c1d94fb5a79970746057de55d089b94d8ee5c671
happygds <dmlab_gds@sjtu.edu.cn>
2019-07-12 17:20:57 +0800

    revise fcn_tap.tex

--- binary_test.py
+++ binary_test.py
@@ -147,7 +147,7 @@ def runner_func(dataset, state_dict, gpu_id, index_queue, result_queue):
                 this_rois, this_actness, this_roi_scores = net(
                     feature, pos_ind, feature_mask=feature_mask, test_mode=True)
                 this_rois, this_actness, this_roi_scores = this_rois.cpu().numpy(
-                ), this_actness.cpu().numpy(), this_roi_scores.cpu().numpy()[:, :, 1] ** (1./3)
+                ), this_actness.cpu().numpy(), this_roi_scores.cpu().numpy()[:, :, 1]
                 this_rois += seg_ind.cpu().numpy().reshape((-1, 1, 1))
                 this_roi_scores *= this_actness
                 
