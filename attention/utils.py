b546fa36c0b488e55ff1228db69ccaca954c1233
happygds <dmlab_gds@sjtu.edu.cn>
2019-07-14 09:34:42 +0800

    revise fcn_tap.tex

--- attention/utils.py
+++ attention/utils.py
@@ -9,7 +9,6 @@ import numpy as np
 
 eps = 1e-10
 
-
 def convert_categorical(x_in, n_classes=2):
     shp = x_in.shape
     x = (x_in.ravel().astype('int'))
@@ -19,7 +18,6 @@ def convert_categorical(x_in, n_classes=2):
     y = y[x] * x_mask
     y = y.reshape(shp + (n_classes,)).astype('float32')
     return y
-
 
 class CE_Criterion_multi(nn.Module):
     def __init__(self, use_weight=True, l_step=1.):
@@ -60,24 +58,18 @@ class CE_Criterion_multi(nn.Module):
             # generate centered matrix
             tsize = target.size()
             H1, H2 = torch.eye(tsize[1], tsize[1]).unsqueeze(0).expand(tsize[0], -1, -1), \
-                (torch.ones((tsize[1], 1)) * torch.ones((1, tsize[1]))
-                 ).unsqueeze(0).expand(tsize[0], -1, -1)
+                (torch.ones((tsize[1], 1)) * torch.ones((1, tsize[1]))).unsqueeze(0).expand(tsize[0], -1, -1)
             H1, H2 = H1.cuda().requires_grad_(False), H2.cuda().requires_grad_(False)
-            H = (H1 - H2 / target.sum(2, keepdim=True).sum(1,
-                                                           keepdim=True).clamp(eps)) * mask.unsqueeze(2) * mask.unsqueeze(1)
+            H = (H1 - H2 / target.sum(2, keepdim=True).sum(1, keepdim=True).clamp(eps)) * mask.unsqueeze(2) * mask.unsqueeze(1)
             target_cov = torch.bmm(target, target.transpose(1, 2))
-            target_cov = torch.bmm(
-                torch.bmm(H, target_cov), H) * mask.unsqueeze(2) * mask.unsqueeze(1)
-
+            target_cov = torch.bmm(torch.bmm(H, target_cov), H) * mask.unsqueeze(2) * mask.unsqueeze(1)
+            
             attn = attn.mean(1)
-            attn = torch.bmm(torch.bmm(H, attn), H) * \
-                mask.unsqueeze(2) * mask.unsqueeze(1)
-            tmp = torch.sqrt((attn * attn).sum(2).sum(1)) * \
-                torch.sqrt((target_cov * target_cov).sum(2).sum(1))
-            tmp_output = 1. - \
-                (attn * target_cov).sum(2).sum(1).clamp(eps) / tmp.clamp(eps)
+            attn = torch.bmm(torch.bmm(H, attn), H) * mask.unsqueeze(2) * mask.unsqueeze(1)
+            tmp = torch.sqrt((attn * attn).sum(2).sum(1)) * torch.sqrt((target_cov * target_cov).sum(2).sum(1))
+            tmp_output = 1. - (attn * target_cov).sum(2).sum(1).clamp(eps) / tmp.clamp(eps)
             attn_output = (tmp_output * mask[:, 0]).mean() * self.l_step ** i
-
+    
         return score_loss, start_loss, end_loss, attn_output
 
 
@@ -87,22 +79,16 @@ class Rank_Criterion(nn.Module):
         self.epsilon = epsilon
 
     def forward(self, x, y, mask):
-        x, y = x[:, :, 1]*mask, y[:, :, 1]*mask
-        # y_max, y_min = y.max(1)[0].unsqueeze(1), y.min(1)[0].unsqueeze(1)
-        # y = (y - y_min) / (y_max - y_min).clamp(eps)
-        # mask = torch.gt(y, 0.).float()
+        x, y = x[:, :, 1], y[:, :, 1]
         mask = mask.unsqueeze(1) * mask.unsqueeze(2)
         y_mask = torch.gt(y.unsqueeze(1) - y.unsqueeze(2), 0.).float()
 
         # pred = F.relu(self.epsilon - (x.unsqueeze(1) - x.unsqueeze(2))) * y_mask * mask
-        pred = (F.relu((y.unsqueeze(1) - y.unsqueeze(2)) - (x.unsqueeze(1) - x.unsqueeze(2)) )) * mask * y_mask
-        # pred = (F.relu(1. - (x.unsqueeze(1) - x.unsqueeze(2)) / (y.unsqueeze(1) - y.unsqueeze(2)).clamp(eps) )) * mask * y_mask
-        output = pred.sum(2).sum(1) / (y_mask * mask).sum(2).sum(1).clamp(eps)
-
-        # pred = (F.relu((y.unsqueeze(1) - y.unsqueeze(2)) - (x.unsqueeze(1) - x.unsqueeze(2)) )) ** 2 * mask
-        # output = pred.sum(2).sum(1) / mask.sum(2).sum(1).clamp(eps)
+        pred = F.relu((y.unsqueeze(1) - y.unsqueeze(2)) - (x.unsqueeze(1) - x.unsqueeze(2))) * y_mask * mask
+        output = torch.abs(pred).sum(2).sum(1) / (y_mask * mask).sum(2).sum(1).clamp(eps)
 
         return output.mean()
+
 
 def position_encoding_init(n_position, d_pos_vec):
     ''' Init the sinusoid position encoding table '''
@@ -117,7 +103,6 @@ def position_encoding_init(n_position, d_pos_vec):
     position_enc[1:, 1::2] = np.cos(position_enc[1:, 1::2])  # dim 2i+1
     return torch.from_numpy(position_enc).type(torch.FloatTensor)
 
-
 def pos_embedding(position_mat, feat_dim, wave_length=10000.):
     feat_range = torch.arange(0, feat_dim / 4)
     dim_mat = torch.pow(wave_length, (4. / feat_dim) * feat_range)
@@ -129,7 +114,6 @@ def pos_embedding(position_mat, feat_dim, wave_length=10000.):
     cos_mat = torch.cos(div_mat)
     embedding = torch.cat([sin_mat, cos_mat], dim=4)
     return embedding.view(pos_size[:3] + (feat_dim,)).float()
-
 
 def roi_embedding(position_mat, feat_dim, wave_length=10000.):
     feat_range = torch.arange(0, feat_dim / 4)
@@ -143,8 +127,7 @@ def roi_embedding(position_mat, feat_dim, wave_length=10000.):
     embedding = torch.cat([sin_mat, cos_mat], dim=3)
     return embedding.view(pos_size[:2] + (feat_dim,)).float()
 
-
-def rank_embedding(position_mat, feat_dim, wave_length=1000.):
+def rank_embedding(position_mat, feat_dim, wave_length=10000.):
     feat_range = torch.arange(0, feat_dim / 2)
     dim_mat = torch.pow(wave_length, (2. / feat_dim) * feat_range)
     dim_mat = dim_mat.view(1, 1, -1).cuda()
@@ -155,7 +138,6 @@ def rank_embedding(position_mat, feat_dim, wave_length=1000.):
     cos_mat = torch.cos(div_mat)
     embedding = torch.cat([sin_mat, cos_mat], dim=2)
     return embedding.view(pos_size[:2] + (feat_dim,)).float()
-
 
 def get_attn_dilated_mask(attn_mask, num_local=16):
     ''' get the dilated mask to utilize the global information '''
@@ -182,8 +164,7 @@ def get_attn_local_mask(attn_mask, num_local=16):
         triu_k, tril_k = num_local // 2, num_local // 2 + 1
     attn_shape = attn_mask.size()
     xx, yy = np.mgrid[0:attn_shape[1], 0:attn_shape[2]]
-    local_mask = np.bitwise_or(
-        xx - yy >= tril_k, yy - xx >= triu_k).astype('uint8')
+    local_mask = np.bitwise_or(xx - yy >= tril_k, yy - xx >= triu_k).astype('uint8')
     local_ind = ((yy - xx) + num_local // 2) * (1. - local_mask)
     local_mask = torch.from_numpy(local_mask).unsqueeze(0).expand(attn_shape)
     local_ind = torch.from_numpy(local_ind)
@@ -192,7 +173,6 @@ def get_attn_local_mask(attn_mask, num_local=16):
         # local_ind = local_ind.cuda().long()
     local_mask = torch.gt(attn_mask + local_mask, 0).requires_grad_(False)
     return local_mask
-
 
 def get_attn_pos(attn_mask, num_local=16):
     ''' Get an attention with relative position embedding.'''
@@ -204,4 +184,4 @@ def get_attn_pos(attn_mask, num_local=16):
     if attn_mask.is_cuda:
         loc_ind = loc_ind.cuda().float().requires_grad_(False)
         mod_ind = mod_ind.cuda().float().requires_grad_(False)
-    return loc_ind, mod_ind
+    return loc_ind, mod_ind        
