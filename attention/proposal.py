72c070700c63e6fd716c72b7ab56ce204f0abad8
happygds <dmlab_gds@sjtu.edu.cn>
2019-07-15 15:08:50 +0800

    revise fcn_tap.tex

--- attention/proposal.py
+++ attention/proposal.py
@@ -24,9 +24,9 @@ def gen_prop(x):
             pstarts[:-1, ], pends[1:, ] - pends[:-1, ]
         # gd_scores = gaussian_filter(diff_scores, bw)
         starts = list(np.nonzero((diff_pstarts[:-1] > 0) & (diff_pstarts[1:] < 0))[
-                      0] + 1) + list(np.nonzero(pstarts > 0.5)[0])
+                      0] + 1) + list(np.nonzero(pstarts > 0.7)[0])
         ends = list(np.nonzero(
-            (diff_pends[:-1] > 0) & (diff_pends[1:] < 0))[0] + 1) + list(np.nonzero(pends > 0.5)[0])
+            (diff_pends[:-1] > 0) & (diff_pends[1:] < 0))[0] + 1) + list(np.nonzero(pends > 0.7)[0])
         starts, ends = list(set(starts)), list(set(ends))
         props = [(x, y, 1, scores[x:y+1].mean()*(pstarts[x]*pends[y]))
                  for x in starts for y in ends if x < y and scores[x:y+1].mean() > min_thre]
@@ -43,10 +43,10 @@ def gen_prop(x):
     # num_keep = int(round(0.125*len(bboxes)))
     # num_keep = min(max(num_keep, rpn_post_nms_top//2), rpn_post_nms_top)
     num_keep = rpn_post_nms_top
-    # if epoch_id is not None and epoch_id < 3:
-    #     bboxes = temporal_nms(bboxes, 0.9)[:num_keep]
-    # else:
-    bboxes = Soft_NMS(bboxes, length=len(scores), max_num=num_keep)
+    if epoch_id is not None and epoch_id < 3:
+        bboxes = temporal_nms(bboxes, 0.9)[:num_keep]
+    else:
+        bboxes = Soft_NMS(bboxes, length=len(scores), max_num=num_keep)
     if len(bboxes) == 0:
         bboxes = [(0, len(scores)-1, 1, scores.mean()*pstarts[0]*pends[-1])]
 
