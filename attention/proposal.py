f863344add5e49c917cbabc40a9e1ca8a3b1808f
happygds <dmlab_gds@sjtu.edu.cn>
2019-07-16 09:40:42 +0800

    revise fcn_tap.tex

--- attention/proposal.py
+++ attention/proposal.py
@@ -113,14 +113,14 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
             gt_k = gts[k]
             gt_k = [x.cpu().numpy() for x in gt_k]
             gt_k = list(filter(lambda b: b[1] > 0 or b[0] < num_feat, gt_k))
-            # sample_infos.append(
-            #     [k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id])
-            _, bboxes_dict[k], rois_iou_dict[k] = gen_prop([k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id])
-        # pool = mp.Pool(processes=8)
-        # handle = [pool.apply_async(gen_prop, args=(
-        #     x,), callback=call_back) for x in sample_infos]
-        # pool.close()
-        # pool.join()
+            sample_infos.append(
+                [k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id])
+            # _, bboxes_dict[k], rois_iou_dict[k] = gen_prop([k, num_feat, scores_k, gt_k, rpn_post_nms_top, epoch_id])
+        pool = mp.Pool(processes=4)
+        handle = [pool.apply_async(gen_prop, args=(
+            x,), callback=call_back) for x in sample_infos]
+        pool.close()
+        pool.join()
 
     for k in range(batch_size):
         bboxes = bboxes_dict[k][:rpn_post_nms_top]
