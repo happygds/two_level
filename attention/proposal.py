7c76b25e3763a9dc2832d5420678fbaa972a92b2
happygds <dmlab_gds@sjtu.edu.cn>
2019-07-17 23:49:46 +0800

    revise fcn_tap.tex

--- attention/proposal.py
+++ attention/proposal.py
@@ -161,7 +161,7 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
                                           rois_cent[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
     rois_relative_pos[:, :, :, 1] = 1. * \
         np.log2((rois_dura[:, :, np.newaxis] / rois_dura[:, np.newaxis, :].clip(1e-14)).clip(1e-14))
-    rois_relative_pos = 1. * \
+    rois_relative_pos = 2. * \
         rois_relative_pos.clip(-16., 16.) * rpn_rois_mask[:, :, np.newaxis,
                                                         np.newaxis] * rpn_rois_mask[:, np.newaxis, :, np.newaxis]
     # import pdb; pdb.set_trace()
