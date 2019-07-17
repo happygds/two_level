29b335d67d8f905d2d8d2a6f40b2ca716d95f9cc
happygds <dmlab_gds@sjtu.edu.cn>
2019-07-17 23:55:34 +0800

    revise fcn_tap.tex

--- ssn_opts.py
+++ ssn_opts.py
@@ -41,7 +41,7 @@ parser.add_argument('--roi_poolsize', type=str, default="1_3")
 parser.add_argument('--num_aug_segments', type=int, default=2)
 parser.add_argument('--num_body_segments', type=int, default=5)
 
-parser.add_argument('--dropout', '--do', default=0.5, type=float,
+parser.add_argument('--dropout', '--do', default=0.1, type=float,
                     metavar='DO', help='dropout ratio (default: 0.5)')
 
 # ========================= Learning Configs ==========================
