ef45eb3d6b517bba1b9ca00e46427bdc6c9f04eb
happygds <dmlab_gds@sjtu.edu.cn>
2019-07-21 20:57:13 +0800

    revise fcn_tap.tex

--- attention/proposal.py
+++ attention/proposal.py
@@ -164,8 +164,8 @@ def proposal_layer(score_output, feature_mask, gts=None, test_mode=False, ss_pro
         rois_cent[:, np.newaxis, :] - rois_cent[:, :, np.newaxis]) / rois_dura[:, np.newaxis, :].clip(1e-14)
     rois_relative_pos[:, :, :, 1] = np.log2(
         (rois_dura[:, :, np.newaxis] / rois_dura[:, np.newaxis, :].clip(1e-14)).clip(1e-14))
-    rois_relative_pos = 5. * \
-        rois_relative_pos.clip(-5., 5.) * rpn_rois_mask[:, :, np.newaxis,
+    rois_relative_pos = 1. * \
+        rois_relative_pos.clip(-16., 16.) * rpn_rois_mask[:, :, np.newaxis,
                                                           np.newaxis] * rpn_rois_mask[:, np.newaxis, :, np.newaxis]
 
     start_rois = torch.from_numpy(
