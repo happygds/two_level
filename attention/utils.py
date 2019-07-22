aafe8a2793c69f8d67b35a0259a5a127130fe8cc
happygds <dmlab_gds@sjtu.edu.cn>
2019-07-22 22:21:30 +0800

    revise fcn_tap.tex

--- attention/utils.py
+++ attention/utils.py
@@ -87,7 +87,7 @@ class Rank_Criterion(nn.Module):
 
         pred = F.relu(self.epsilon - (x.unsqueeze(1) - x.unsqueeze(2))) * y_mask * mask
         # pred = F.relu((y.unsqueeze(1) - y.unsqueeze(2)) - (x.unsqueeze(1) - x.unsqueeze(2))) * y_mask * mask
-        output = torch.abs(pred).sum(2).sum(1) / (y_mask * mask).sum(2).sum(1).clamp(eps)
+        output = pred.sum(2).sum(1) / (y_mask * mask).sum(2).sum(1).clamp(eps)
 
         return output.mean()
 
