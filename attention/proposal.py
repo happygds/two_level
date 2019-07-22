9d065dc7db89c969093c54ee761f50ed8228b986
happygds <dmlab_gds@sjtu.edu.cn>
2019-07-22 08:35:03 +0800

    revise fcn_tap.tex

--- attention/proposal.py
+++ attention/proposal.py
@@ -41,19 +41,17 @@ def gen_prop(x):
     # num_keep = int(round(0.125*len(bboxes)))
     # num_keep = min(max(num_keep, rpn_post_nms_top//2), rpn_post_nms_top)
     num_keep = rpn_post_nms_top
-    if len(bboxes) == 0:
-        bboxes = [(0, len(scores), 1, scores.mean()*pstarts[0]*pends[-1])]
     if epoch_id is not None and epoch_id < 3:
         bboxes = temporal_nms(bboxes, 0.9)[:num_keep]
     else:
         bboxes = Soft_NMS(bboxes, length=len(scores), max_num=num_keep)
+    if len(bboxes) == 0:
+        bboxes = [(0, len(scores), 1, scores.mean()*pstarts[0]*pends[-1])]
 
     # compute iou with ground-truths
     if gt_k is not None:
         if len(gt_k) == 0:
             gt_k = [(0, 1)]
-        if len(bboxes) == 0:
-            bboxes = [(0, len(scores), 1, scores.mean()*pstarts[0]*pends[-1])]
         rois = [(x[0], x[1]) for x in bboxes]
         gt_k, rois = np.asarray(gt_k), np.asarray(rois)
         rois_iou = wrapper_segment_iou(gt_k, rois).max(axis=1).reshape((-1, 1))
