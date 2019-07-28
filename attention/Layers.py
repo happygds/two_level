b9d27bf1340cb2aa56a106bd3ad5c74f9749f5b9
happygds <dmlab_gds@sjtu.edu.cn>
2019-07-29 01:07:02 +0800

    revise fcn_tap.tex

--- attention/Layers.py
+++ attention/Layers.py
@@ -115,7 +115,7 @@ class ROI_Relation(nn.Module):
         self.right_fc = nn.Sequential(nn.Linear(
             (self.bpool_size+roipool_size//2+1)*d_model, d_model), nn.Dropout(dropout), nn.SELU())
         self.roi_fc = nn.Sequential(
-            nn.Linear(3*d_model, d_model), nn.Dropout(dropout), nn.SELU())
+            nn.Linear(2*d_model, d_model), nn.Dropout(dropout), nn.SELU())
 
     def forward(self, features, start_rois, end_rois, rois, rois_mask, rois_pos_emb):
         len_feat = features.size()[1]
@@ -132,7 +132,7 @@ class ROI_Relation(nn.Module):
             self.roipool_size+self.bpool_size)].contiguous().view(roi_feat_size[:2]+(-1,)))
         right_feats = self.left_fc(roi_feats[:, :, :, (self.roipool_size//2+self.bpool_size):(
             self.roipool_size+2*self.bpool_size)].contiguous().view(roi_feat_size[:2]+(-1,)))
-        roi_feats = self.roi_fc(torch.cat([left_feats, inner_feats, right_feats], dim=2))
+        roi_feats = self.roi_fc(torch.cat([right_feats - left_feats, inner_feats], dim=2))
 
         # compute mask
         mb_size, len_k = roi_feats.size()[:2]
