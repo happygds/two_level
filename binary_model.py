78fa2003fa22d77afc1f3235ea686c4554881831
happygds <dmlab_gds@sjtu.edu.cn>
2019-08-02 21:18:09 +0800

    revise fcn_tap.tex

--- binary_model.py
+++ binary_model.py
@@ -47,7 +47,7 @@ class BinaryClassifier(torch.nn.Module):
 
         self.roi_relations = ROI_Relation(args.d_model, args.roi_poolsize, args.d_inner_hid,
                                           args.n_head, args.d_k, args.d_v, dropout=self.dropout)
-        # self.norm = nn.BatchNorm1d(args.d_model)
+        self.norm = nn.BatchNorm1d(args.d_model)
         # self.roi_feat= nn.Sequential(
         #     nn.Linear(args.d_model, args.d_model), nn.SELU(), nn.Dropout(self.dropout))
         # self.w_roi = nn.Parameter(torch.FloatTensor(1, 1, args.d_model))
@@ -108,7 +108,7 @@ class BinaryClassifier(torch.nn.Module):
         rois_pos_emb = pos_embedding(rois_relative_pos, self.d_model)
         roi_feats = self.roi_relations(
             enc_input, start_rois, end_rois, rois, rois_mask, rois_pos_emb)
-        # roi_feats = self.norm(roi_feats.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
+        roi_feats = self.norm(roi_feats.transpose(1, 2).contiguous()).transpose(1, 2).contiguous()
         # roi_feat_max = self.roi_feat_max(roi_feats).max(1)[0].unsqueeze(1)
         # roi_feat_max = self.w_roi
         roi_scores = F.softmax(self.roi_cls(roi_feats), dim=2)
