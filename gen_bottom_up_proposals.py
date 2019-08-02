8beeba01a93e50cec3b168e4839f3e5fb9c3d430
happygds <dmlab_gds@sjtu.edu.cn>
2019-08-02 19:40:32 +0800

    revise fcn_tap.tex

--- gen_bottom_up_proposals.py
+++ gen_bottom_up_proposals.py
@@ -152,7 +152,7 @@ def gen_prop(v):
     # bboxes = Soft_NMS(bboxes, length=frm_cnt)
 
     if len(bboxes) == 0:
-        bboxes = [(0, float(v.frame_cnt) / v.frame_interval, 1, 1)]
+        bboxes = [(0, v.duration, 1, 1)]
 
     pr_box = [(x[0] / float(frm_cnt) * v.duration, x[1] / float(frm_cnt) * v.duration) for x in bboxes]
     # pr_box = [(x[0] * v.frame_interval / float(v.frame_cnt) * v.duration, x[1] * v.frame_interval / float(v.frame_cnt) * v.duration) for x in bboxes]
