783b9335b197125aef66bfb06819a4787d2f939c
happygds <dmlab_gds@sjtu.edu.cn>
2019-08-05 09:03:16 +0800

    revise fcn_tap.tex

--- binary_train.py
+++ binary_train.py
@@ -301,10 +301,12 @@ def validate(val_loader, model, criterion_stage1, criterion_stage2, iter, epoch)
             for k, v in enumerate(this_rois):
                 video_info = val_loader.dataset.video_list[index[k]]
                 video_id, fps = video_info.id, video_info.fps
+                v = list(filter(lambda b: b[1] - b[0] > 0, v))
+                k_roi_scores = list(filter(lambda b: b > 0, this_roi_scores[k]))
                 video_lst.extend([video_id] * len(v))
                 t_start_lst.extend([x[0] * 5 / fps for x in v])
                 t_end_lst.extend([x[1] * 5 / fps for x in v])
-                score_lst.extend([x for x in this_roi_scores[k]])
+                score_lst.extend([x for x in k_roi_scores])
 
     prediction = pd.DataFrame({'video-id': video_lst,
                         't-start': t_start_lst,
